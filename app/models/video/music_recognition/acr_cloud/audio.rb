904fa5a227f660908fdafebb0918a90741ecbe40
Justin Marsh <jmarsh24@gmail.com>
2021-12-27 10:50:18 +0100

    Change musicmatchworker to after video import

--- app/models/video/music_recognition/acr_cloud/audio.rb
+++ app/models/video/music_recognition/acr_cloud/audio.rb
@@ -31,7 +31,7 @@ class Video::MusicRecognition::AcrCloud::Audio
   end
 
   def youtube_audio_file_path
-    ("." + RELATIVE_SAVE_PATH + @youtube_id + OUTPUT_FORMAT).to_s
+    ".#{RELATIVE_SAVE_PATH + @youtube_id + OUTPUT_FORMAT}"
   end
 
   def output_file_path
