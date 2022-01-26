84981bc02a7fc9fee68d7d06192e41adaa179e39
Justin Marsh <jmarsh24@gmail.com>
2022-01-26 18:54:45 +0100

    Add events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,29 @@
 class EventsController < ApplicationController
   def index
-    @events =
-      Event.title_search(params[:q]).distinct.order(:title).pluck(:title, :id)
-    render json: @events
+    respond_to do
+      |format|
+        format.html { @events = Event.all.order(:id)}
+        format.json { render json: @events =  Event.title_search(params[:q])
+                                                    .distinct
+                                                    .order(:title)
+                                                    .pluck(:title,
+                                                            :id)}
+    end
+  end
+
+  def create
+    @event = Event.create(  title: params[:event][:title],
+                            city: params[:event][:city],
+                            country: params[:event][:country]
+                            )
+    if @event.save
+      redirect_to root_path,
+                  notice:
+                    "Event Sucessfully Added"
+    else
+      redirect_to events_path,
+      notice:
+        "Event not saved."
+    end
   end
 end
