28a00f0003b27911866c975358f7417482cb0fe3
Justin Marsh <jmarsh24@gmail.com>
2022-02-01 20:28:56 +0100

    Add find videos when new leader or follower is added

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -11,7 +11,9 @@ class FollowersController < ApplicationController
   end
 
   def create
-    @follower = Follower.create(name: "#{params[:follower][:first_name]} #{params[:follower][:last_name]}", first_name: params[:follower][:first_name], last_name: params[:follower][:last_name])
+    @follower = Follower.create(  name: "#{params[:follower][:first_name]} #{params[:follower][:last_name]}",
+                                  first_name: params[:follower][:first_name],
+                                  last_name: params[:follower][:last_name])
     fetch_new_follower
 
     redirect_to root_path,
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -8,4 +8,12 @@ class Follower < ApplicationRecord
   has_many :videos
   has_many :leader, through: :videos
   has_many :song, through: :videos
+
+  after_create :find_videos
+
+  def find_videos
+    Video.with_dancer_name_in_title(name).find_each do |video|
+      video.update(follower: self)
+    end
+  end
 end
