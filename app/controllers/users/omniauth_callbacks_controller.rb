dbe3ba063e98ace5cfd26de34a0c732c20b0fab3
Justin Marsh <jmarsh24@gmail.com>
2022-02-08 01:32:22 +0100

    add missing callback controller

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,20 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    handle_with "Google"
+  end
+
+  def github
+    handle_auth "Github"
+  end
+
+  def handle_auth(kind)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+    else
+      flash[:error]='There was a problem signing you in through Github. Please register or try signing in later.'
+      redirect_to new_user_registration_url
+    end
+  end
+end
