d48d233bdb3a83c91ef445975083f01b90fe5a75
Justin Marsh <jmarsh24@gmail.com>
2022-02-08 02:15:04 +0100

    add image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,20 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.where(email: access_token.info.email).first
     unless user
       user = User.create(
-          email: data['email'],
+          email: access_token.info.email,
           password: Devise.friendly_token[0,20]
       )
     end
+    user.name = access_token.info.name
+    user.first_name = access_token.info.first_name
+    user.last_name = access_token.info.last_name
+    user.image = access_token.info.image
+    user.uid = access_token.uid
+    user.provider = access_token.info.provider
+    user.save
     user
   end
 end
