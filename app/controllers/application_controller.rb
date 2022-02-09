4e5eab086104ae6476348bdeacbfada82f3a7685
Justin Marsh <jmarsh24@gmail.com>
2022-02-09 01:50:06 +0100

    add first and last name permission to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   after_action :track_action
   before_action :set_total_videos_count
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
@@ -16,5 +17,11 @@ class ApplicationController < ActionController::Base
   def track_action
     ahoy.track "Ran action", request.params
   end
-  
+
+  def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+
+       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  end
+
 end
