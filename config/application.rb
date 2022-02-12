a5293d8e45c8f454692e4752ae5910018715bb65
Justin Marsh <jmarsh24@gmail.com>
2022-02-12 05:18:03 +0100

    update to rails 7.0.2

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
-require_relative 'boot'
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 require 'openssl'
 require 'base64'
 require 'net/http/post/multipart'
@@ -16,18 +17,17 @@ end
 module TangoLibrary
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.assets.paths << Rails.root.join('app', 'assets', 'img')
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
     config.autoload_paths += %W[#{config.root}/lib]
-
     config.active_record.schema_format = :sql
-
     config.active_job.queue_adapter = :sidekiq
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,5 @@
 # Load the Rails application.
-require_relative 'application'
+require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails
-  .application
-  .reloader
-  .to_prepare do
-    # Autoload classes and modules needed at boot time here.
-  end
