21845b7ae817d1186282dca72feff90c4d528c59
Justin Marsh <jmarsh24@gmail.com>
2022-02-12 05:26:36 +0100

    add webpacker to rails 7.0.2 upgrade

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,3 @@
-const { environment } = require("@rails/webpacker");
-const jquery = require("./plugins/jquery");
+const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend("jquery", jquery);
-module.exports = environment;
+module.exports = environment
