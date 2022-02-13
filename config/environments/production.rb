e9fedcb366684847cfd79dceeb2574dcfdf5e6cc
Justin Marsh <jmarsh24@gmail.com>
2022-02-13 22:31:44 +0100

    fix user login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => "apikey",
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'tangotube.tv',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options ={:host => 'tangotube.tv', :protocol => 'https'}
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
