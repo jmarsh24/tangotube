24f4e9ef1b662a1488a8c96e9a808a374bbbdd0b
Justin Marsh <jmarsh24@gmail.com>
2022-02-19 14:19:16 +0100

    fix &amp; in title

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -46,13 +46,13 @@ module VideosHelper
               watch_path(v: youtube_id),
               { 'data-turbo-frame': "_top" }
     end
-  end
+  end``
 
   def primary_title(dancer_names, title, song_attributes, _youtube_id)
     if dancer_names.present? && song_attributes.present?
       dancer_names
     else
-      truncate(title, length: 85)
+      title
     end
   end
 
