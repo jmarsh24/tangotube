fce889add1c9cbadbb20a4490f319fceee078913
Justin Marsh <jmarsh24@gmail.com>
2022-02-19 19:46:09 +0100

    add popover for like buttons?

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 require 'barnes'
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 1)
 threads threads_count, threads_count
 
