1e868f100f1cade25cb815e8714a84bd720fa37d
Justin Marsh <jmarsh24@gmail.com>
2022-02-23 18:03:21 +0100

    working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,15 +43,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:user_name),
-    :password => Rails.application.credentials.dig(:password),
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
-  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Print deprecation notices to the Rails logger.
