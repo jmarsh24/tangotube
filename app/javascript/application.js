eb0faefb26d4d52bf9d63bf1e9ab1f250d03cf66
Marco Roth <marco.roth@intergga.ch>
2022-02-25 20:07:23 +0100

    Setup Turbo on the client-side that the Turbo Streams can register subscriptions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Entry point for the build script in your package.json
-require("@hotwired/turbo")
-
 import "@fortawesome/fontawesome-free/js/all"
+
+import "./channels"
 import "./controllers"
+
+import { Turbo } from "@hotwired/turbo-rails"
 
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
