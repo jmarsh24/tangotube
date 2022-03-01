525c92d292da0fb8948a09741cc6b234140f10f1
Justin Marsh <jmarsh24@gmail.com>
2022-03-01 18:49:10 +0100

    rebase

--- app/javascript/controllers/reply_controller.js
+++ app/javascript/controllers/reply_controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
@@ -7,28 +6,5 @@ export default class extends Controller {
   toggle(event) {
     event.preventDefault();
     this.formTarget.classList.toggle("isHidden");
-=======
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = [ "form" ]
-
-  //connect() {
-    //this.outputTarget.textContent = 'Hello, Stimulus!'
-  //}
-
-  toggle(event) {
-    event.preventDefault()
-    this.formTarget.classList.toggle("isHidden")
->>>>>>> bbd9037afad8b97e909dc091022630e97cd9486b
   }
 }
