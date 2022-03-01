25b68bb29710d3163fc6dc773fedd3c769ce8021
Justin Marsh <jmarsh24@gmail.com>
2022-03-01 18:47:04 +0100

    Fix rebase

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,23 +1,29 @@
 class Comment < ApplicationRecord
-  include ActionView::RecordIdentifier
-
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
-  has_many :comments, foreign_key: :parent_id, dependent: :destroy
 
-  validates :body, presence: true
-
-  after_create_commit do
-    broadcast_append_to [commentable, :comments], target: "#{dom_id(parent || commentable)}_comments", partial: "comments/comment_with_replies"
+  def comments
+    Comment.includes([:commentable]).where(commentable: commentable, parent_id: id)
   end
 
-  after_update_commit do
-    broadcast_replace_to self
+  def parent_comments
+    Comment.where(commentable: commentable, id: parent_id)
   end
 
-  after_destroy_commit do
-    broadcast_remove_to self
-    broadcast_action_to self, action: :remove, target: "#{dom_id(self)}_with_comments"
+  def destroy
+    update(user: nil, body: nil)
+    parent_comments.each do |comment|
+      if comment.deleted?
+        comment.delete
+      end
+    end
+    if parent_id.nil? || comments.empty?
+      delete
+    end
+  end
+
+  def deleted?
+    user.nil?
   end
 end
