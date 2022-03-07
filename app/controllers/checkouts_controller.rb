bed46d10c69b0e15ab1644866e0d2232d346c085
Justin Marsh <jmarsh24@gmail.com>
2022-03-07 13:35:49 +0100

    Add stripe views

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,7 +4,15 @@ class CheckoutsController < ApplicationController
   def show
     current_user.set_payment_processor :stripe
     current_user.pay_customers
-    @checkout_session = current_user.payment_processor.checkout(
+    @tier_1 = current_user.payment_processor.checkout(
+      mode: "subscription",
+      line_items: "price_1Kaet3COXOTU8ZRQua1CoSnf"
+    )
+    @tier_2 = current_user.payment_processor.checkout(
+      mode: "subscription",
+      line_items: "price_1KaetrCOXOTU8ZRQoEnehPK8"
+    )
+    @tier_3 = current_user.payment_processor.checkout(
       mode: "subscription",
       line_items: "price_1KacxjCOXOTU8ZRQbvw5lzB1"
     )
