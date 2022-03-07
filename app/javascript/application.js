409c2e7c7636fff991730818c425adb4b063f231
Justin Marsh <jmarsh24@gmail.com>
2022-03-07 20:23:29 +0100

    Add tailwind

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,12 @@
-require("@hotwired/turbo-rails")
+require("@hotwired/turbo-rails");
 
-import "@fortawesome/fontawesome-free/js/all"
+import "@fortawesome/fontawesome-free/js/all";
 
-import "./channels"
-import "./controllers"
+import "./channels";
+import "./controllers";
 
 // Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
