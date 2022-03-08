c1d40a203cf2185d26a63189990cff3969b26371
Justin Marsh <jmarsh24@gmail.com>
2022-03-08 16:04:11 +0100

    fix show view

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,53 +1,67 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-static targets = ["filterButton",
-                  "filter",
-                  "toggleable",
-                  "sideNavContainer",
-                  "mainSectionContainer",
-                  "disableable",
-                  "lyricsEs",
-                  "lyricsEn",
-                  "recommendedSongVideos",
-                  "recommendedSongVideosDownButton",
-                  "recommendedSongVideosUpButton",
-                  "recommendedEventVideos",
-                  "recommendedEventVideosDownButton",
-                  "recommendedEventVideosUpButton",
-                  "recommendedChannelVideos",
-                  "recommendedChannelVideosDownButton",
-                  "recommendedChannelVideosUpButton",
-                  "recommendedPerformanceVideos",
-                  "recommendedPerformanceVideosDownButton",
-                  "recommendedPerformanceVideosUpButton"];
+  static targets = [
+    "filterButton",
+    "filter",
+    "toggleable",
+    "sideNavContainer",
+    "mainSectionContainer",
+    "disableable",
+    "lyricsEs",
+    "lyricsEn",
+    "recommendedSongVideos",
+    "recommendedSongVideosDownButton",
+    "recommendedSongVideosUpButton",
+    "recommendedEventVideos",
+    "recommendedEventVideosDownButton",
+    "recommendedEventVideosUpButton",
+    "recommendedChannelVideos",
+    "recommendedChannelVideosDownButton",
+    "recommendedChannelVideosUpButton",
+    "recommendedPerformanceVideos",
+    "recommendedPerformanceVideosDownButton",
+    "recommendedPerformanceVideosUpButton",
+  ];
 
   toggle() {
     this.toggleableTarget.classList.toggle("isHidden");
   }
 
   toggleRecommendedSongVideos() {
-    this.recommendedSongVideosTarget.classList.toggle("recommended-videos-card--active")
+    this.recommendedSongVideosTarget.classList.toggle(
+      "recommended-videos-card--active"
+    );
     this.recommendedSongVideosDownButtonTarget.classList.toggle("isHidden");
     this.recommendedSongVideosUpButtonTarget.classList.toggle("isHidden");
   }
 
   toggleRecommendedEventVideos() {
-    this.recommendedEventVideosTarget.classList.toggle("recommended-videos-card--active")
+    this.recommendedEventVideosTarget.classList.toggle(
+      "recommended-videos-card--active"
+    );
     this.recommendedEventVideosDownButtonTarget.classList.toggle("isHidden");
     this.recommendedEventVideosUpButtonTarget.classList.toggle("isHidden");
   }
 
   toggleRecommendedChannelVideos() {
-    this.recommendedChannelVideosTarget.classList.toggle("recommended-videos-card--active")
+    this.recommendedChannelVideosTarget.classList.toggle(
+      "recommended-videos-card--active"
+    );
     this.recommendedChannelVideosDownButtonTarget.classList.toggle("isHidden");
     this.recommendedChannelVideosUpButtonTarget.classList.toggle("isHidden");
   }
 
   toggleRecommendedPerformanceVideos() {
-    this.recommendedPerformanceVideosTarget.classList.toggle("recommended-videos-card--active")
-    this.recommendedPerformanceVideosDownButtonTarget.classList.toggle("isHidden");
-    this.recommendedPerformanceVideosUpButtonTarget.classList.toggle("isHidden");
+    this.recommendedPerformanceVideosTarget.classList.toggle(
+      "recommended-videos-card--active"
+    );
+    this.recommendedPerformanceVideosDownButtonTarget.classList.toggle(
+      "isHidden"
+    );
+    this.recommendedPerformanceVideosUpButtonTarget.classList.toggle(
+      "isHidden"
+    );
   }
 
   toggleLyricsSpanish() {
@@ -66,15 +80,20 @@ static targets = ["filterButton",
   }
 
   navShowHide() {
-      this.sideNavContainerTarget.classList.toggle('isHidden')
-      this.mainSectionContainerTarget.classList.toggle('leftPadding')
+    this.sideNavContainerTarget.classList.toggle("isHidden");
   }
 
   disableFilters() {
-    const ssfilterListItems = document.getElementsByClassName("ss-list")
-    const ssfilterContainer = document.getElementsByClassName('ss-content ss-open')
+    const ssfilterListItems = document.getElementsByClassName("ss-list");
+    const ssfilterContainer = document.getElementsByClassName(
+      "ss-content ss-open"
+    );
 
-    Array.from(ssfilterListItems).forEach(element => element.classList.toggle('disabled'))
-    Array.from(ssfilterContainer).forEach(element => element.classList.toggle('disabled'))
+    Array.from(ssfilterListItems).forEach((element) =>
+      element.classList.toggle("disabled")
+    );
+    Array.from(ssfilterContainer).forEach((element) =>
+      element.classList.toggle("disabled")
+    );
   }
 }
