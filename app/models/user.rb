8c8a7d5f1a379db4ab14ed4140365cb5cac6402e
Justin Marsh <jmarsh24@gmail.com>
2022-03-09 13:38:49 +0100

    remove password compexity requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validate :password_complexity
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -32,15 +31,6 @@ class User < ApplicationRecord
   def set_default_role
     self.role ||= :user
   end
-
-  def password_complexity
-    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
-
-    errors.add :password,
-    "Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character"
-  end
-
 
   def tileize_name
     self.name = name.titleize if name.present?
