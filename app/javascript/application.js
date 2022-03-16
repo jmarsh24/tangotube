13f77579bc9740594000c95dea66ca6e17d4321d
Justin Marsh <jmarsh24@gmail.com>
2022-03-16 23:21:30 +0100

    working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,7 @@ import "@fortawesome/fontawesome-free/js/all";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 setBasePath("/shoelace-assets");
 
-import "@shoelace-style/shoelace/dist/components/button/button.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
+import "@shoelace-style/shoelace";
 
 import "./channels";
 import "./controllers";
