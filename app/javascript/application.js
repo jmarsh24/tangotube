b6e02ca284de35915245ac82f3562c292f9fd9c4
Justin Marsh <jmarsh24@gmail.com>
2022-03-17 21:47:33 +0100

    Load shoelace via gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,11 @@ import "./channels";
 import "./controllers";
 import "@fortawesome/fontawesome-free/js/all";
 
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
+setBasePath("/shoelace-assets");
+
+import "@shoelace-style/shoelace";
+
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
