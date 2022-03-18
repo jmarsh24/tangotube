b7be2c7ea211dccea1567b591e6b21a4df9e02ec
Justin Marsh <jmarsh24@gmail.com>
2022-03-18 20:25:46 +0100

    Only require necessary js from shoelace

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,7 +7,16 @@ import "@fortawesome/fontawesome-free/js/all";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 setBasePath("/shoelace-assets");
 
-import "@shoelace-style/shoelace";
+import "@shoelace-style/shoelace/dist/components/dropdown/dropdown.js";
+import "@shoelace-style/shoelace/dist/components/avatar/avatar.js";
+import "@shoelace-style/shoelace/dist/components/alert/alert.js";
+import "@shoelace-style/shoelace/dist/components/drawer/drawer.js";
+import "@shoelace-style/shoelace/dist/components/button/button.js";
+import "@shoelace-style/shoelace/dist/components/divider/divider.js";
+import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+import "@shoelace-style/shoelace/dist/components/menu/menu.js";
+import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js";
+import "@shoelace-style/shoelace/dist/components/switch/switch.js";
 
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement("script");
