3eacc99d064dcb498837e860f9a994424d0e5bc0
Justin Marsh <jmarsh24@gmail.com>
2022-03-24 12:52:50 +0100

    remove blank user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,17 @@ module ApplicationHelper
   def avatar_url_for(user, opts = {})
     size = opts[:size || 32]
 
-    if user.avatar.attached?
-      user.avatar.variant(:thumb)
-    elsif user.image?
-      user.image
+    if user.present?
+      if user.avatar.attached?
+        user.avatar.variant(:thumb)
+      elsif user.image?
+        user.image
+      else
+        hash = Digest::MD5.hexdigest(user.email.downcase)
+        "https://secure.gravatar.com/avatar/#{hash}.png?s=#{size}"
+      end
     else
-      hash = Digest::MD5.hexdigest(user.email.downcase)
-      "https://secure.gravatar.com/avatar/#{hash}.png?s=#{size}"
+      image_path("blank_user")
     end
- end
+  end
 end
