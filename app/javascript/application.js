b365a5e1bbc498546def381afad24a5f91e98c8e
Justin Marsh <jmarsh24@gmail.com>
2022-03-31 15:22:43 +0200

    define menu items

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,6 +16,8 @@ import "@shoelace-style/shoelace/dist/components/divider/divider.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
 import "@shoelace-style/shoelace/dist/components/menu/menu.js";
 import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js";
+import "@shoelace-style/shoelace/dist/components/menu-label/menu-label.js";
+import "@shoelace-style/shoelace/dist/components/menu/menu.js";
 import "@shoelace-style/shoelace/dist/components/switch/switch.js";
 import "@shoelace-style/shoelace/dist/components/tag/tag.js";
 
