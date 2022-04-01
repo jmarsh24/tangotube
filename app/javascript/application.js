c1a7f136856b7b722547f3036dccea6d99f1efb3
Justin Marsh <jmarsh24@gmail.com>
2022-04-01 14:12:52 +0200

    Add spinner and end of results

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -18,6 +18,7 @@ import "@shoelace-style/shoelace/dist/components/menu/menu.js";
 import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js";
 import "@shoelace-style/shoelace/dist/components/menu-label/menu-label.js";
 import "@shoelace-style/shoelace/dist/components/menu/menu.js";
+import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
 import "@shoelace-style/shoelace/dist/components/switch/switch.js";
 import "@shoelace-style/shoelace/dist/components/tag/tag.js";
 
