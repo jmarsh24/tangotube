682dc6adf30c0c4061e38d10ac111ba34c9585db
Justin Marsh <jmarsh24@gmail.com>
2022-04-04 16:29:53 +0200

    Add redis cloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,5 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+if ENV["REDISCLOUD_URL"]
+  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
 
 Searchkick.redis = ConnectionPool.new { Redis.new }
