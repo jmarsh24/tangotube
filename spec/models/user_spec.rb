eb2162fbfedad8119b221c648142de8fcfc9a4de
Justin Marsh <jmarsh24@gmail.com>
2022-04-08 06:31:50 +0200

    Fix login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,49 +5,54 @@ RSpec.describe User, type: :model do
     let(:user) { build(:user) }
 
     it "is valid with valid attributes" do
-      expect(user.save).to eq(true)
+      user.first_name = "John"
+      user.last_name = "Doe"
+      user.email = "sample@example.com"
+      user.password = "password"
+      expect(user.save).to be(true)
     end
 
     it "is not valid without a password" do
       user.password = nil
-      expect(user.save).to eq(false)
+      expect(user.save).to be(false)
     end
 
     it "is not valid without an email" do
       user.email = nil
-      expect(user.save).to eq(false)
+      expect(user.save).to be(false)
     end
 
-    # it 'ensures last name presence' do
-    #   user = User.new(first_name: 'First', email: 'sample@example.com').save
-    #   expect(user).to eq(false)
-    # end
+    it "ensures last name presence" do
+      user = User.new(first_name: "First", email: "sample@example.com", password: "password").save
+      expect(user).to be(false)
+    end
 
-    # it 'ensures last name presence' do
-    #   user = User.new(first_name: 'First', last_name: 'Last').save
-    #   expect(user).to eq(false)
-    # end
+    it "ensures last name presence" do
+      user = User.new(first_name: "First", last_name: "Last", password: "password").save
+      expect(user).to be(false)
+    end
 
-    # it 'ensures last name presence' do
-    #   user = User.new(first_name: 'First', last_name: 'Last', email: 'sample@example.com').save
-    #   expect(user).to eq(true)
-    # end
+    it "ensures last name presence" do
+      user = User.new(first_name: "First", last_name: "Last", email: "sample@example.com", password: "password").save
+      expect(user).to be(true)
+    end
 
-    # context 'scope tests' do
-    #   let(:params) { { first_name: 'First', last_name: 'Last', email: 'sample@example.com' } }
-    #   before(:each) do
+    # context "scope tests" do
+    #   let(:params) { { first_name: "First", last_name: "Last", email: "sample@example.com" } }
+
+    #   before do
     #     User.new(params).save
     #     User.new(params).save
     #     User.new((params).merge(active: true)).save
     #     User.new((params).merge(active: false)).save
     #     User.new((params).merge(active: false)).save
     #   end
-
-    #   it 'should return active users' do
-    #     expect(User.active_users.size).to eq(3)
-    #   end
-    #   it 'should return inactive users' do
-    #     expect(User.inactive_users.size).to eq(2)
-    #   end
+    # end
+      # it 'should return active users' do
+      #   expect(User.active_users.size).to eq(3)
+      # end
+      # it 'should return inactive users' do
+      #   expect(User.inactive_users.size).to eq(2)
+      # end
   end
 end
