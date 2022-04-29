8f46166d00c00b99822952126d46202e7aa85099
Justin Marsh <jmarsh24@gmail.com>
2022-04-29 02:05:24 +0200

    Add stimulus controller and clean up views

---
+++ app/javascript/controllers/banner_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+import Swiper, { Navigation, Pagination } from "swiper";
+
+// Connects to data-controller="banner"
+export default class extends Controller {
+  static targets: ["banner"];
+
+  connect() {
+    swiper = new Swiper(this.bannerTarget, {
+      modules: [Navigation, Pagination],
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: true,
+      },
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+}
