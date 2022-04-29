581ba730d0930397500bac2887bdab812596dcab
Justin Marsh <jmarsh24@gmail.com>
2022-04-29 13:08:50 +0200

    Remove robots.txt

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module TangoTube
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.middleware.use Rack::XRobotsTag
   end
 
 
