ad3a9e558f2a36eefdbe277c2be3d1b2ee0fcae6
Justin Marsh <jmarsh24@gmail.com>
2022-05-01 12:24:08 +0200

    Fix naming of migration constant

--- db/migrate/20220501102011_add_featured_again_to_videos.rb
+++ db/migrate/20220501102011_add_featured_again_to_videos.rb
@@ -1,4 +1,4 @@
-class AddFeaturedToVideos < ActiveRecord::Migration[7.0]
+class AddFeaturedAgainToVideos < ActiveRecord::Migration[7.0]
   def change
     add_column :videos, :featured, :boolean, default: :false, index: true
   end
