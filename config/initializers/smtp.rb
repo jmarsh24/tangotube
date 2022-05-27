5c9c9cfb4300924023ef317bb29d7a2cb0980381
Justin Marsh <jmarsh24@gmail.com>
2022-05-27 23:36:19 +0200

    Update sendgrid initializer

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  domain:         "smtp.sendgrid.net",
+  domain:         "tangotube.tv",
   address:        "smtp.sendgrid.net",
   port:            587,
   authentication: :plain,
-  user_name:      'apikey',
+  user_name:      "apikey",
+  enable_starttls_auto: true,
   password:       Rails.application.credentials.dig(:sendgrid, :api_key)
 }
