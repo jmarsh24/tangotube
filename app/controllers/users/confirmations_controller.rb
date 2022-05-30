04c9e32e019d695ba336c8eec9df9a36e7b76d4b
Justin Marsh <jmarsh24@gmail.com>
2022-05-30 04:39:42 +0200

    Fix redirect after login

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,8 +1,8 @@
-class ConfirmationsController < Devise::ConfirmationsController
+class Users::ConfirmationsController < Devise::ConfirmationsController
 
   private
 
-  def after_confirmation_path_for(resource_name, resource)
+  def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
     root_path
   end
