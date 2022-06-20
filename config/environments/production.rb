a96332fa14c93276a7219d18811af914f0306772
Justin Marsh <jmarsh24@gmail.com>
2022-06-20 02:35:19 +0200

    redis_host instead of redis_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.cache_store =
     :redis_cache_store,
-    { url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' } }
+    { url: ENV.fetch('REDIS_HOST') { 'redis://localhost:6379/1' } }
   config.session_store :cache_store,
                       key: '_session',
                       compress: true,
