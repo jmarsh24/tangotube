1cfb1f7f113ab2692a00457720f1d929b34e0589
Justin Marsh <jmarsh24@gmail.com>
2022-06-20 16:22:45 +0200

    remove yt-dlp from build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,30 +1,29 @@
 set -o errexit
 
-indent() {
-  sed -u 's/^/       /'
-}
+# indent() {
+#   sed -u 's/^/       /'
+# }
 
-echo "-----> Installing yt-dlp"
-cd $HOME
-mkdir -p "vendor"
-cd "vendor"
-mkdir -p ./yt-dlp/bin/
-cd ./yt-dlp/bin/
-wget $(curl -s https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest \
-| grep -e "browser_download_url.*yt-dlp" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| grep -e "yt-dlp$")
-chmod a+rx yt-dlp
-echo "Adding to PATH" | indent
+# echo "-----> Installing yt-dlp"
+# cd $HOME
+# mkdir -p "vendor"
+# cd "vendor"
+# mkdir -p ./yt-dlp/bin/
+# cd ./yt-dlp/bin/
+# wget $(curl -s https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest \
+# | grep -e "browser_download_url.*yt-dlp" \
+# | cut -d : -f 2,3 \
+# | tr -d \" \
+# | grep -e "yt-dlp$")
+# chmod a+rx yt-dlp
+# echo "Adding to PATH" | indent
 
-PROFILE="$HOME/.profile.d/yt-dlp.sh"
-mkdir -p "$HOME/.profile.d/"
-touch $PROFILE
-export PATH="$PATH:$HOME/vendor/yt-dlp/bin" >> $PROFILE
-source $PROFILE
+# PROFILE="$HOME/.profile.d/yt-dlp.sh"
+# mkdir -p "$HOME/.profile.d/"
+# touch $PROFILE
+# export PATH="$PATH:$HOME/vendor/yt-dlp/bin" >> $PROFILE
 
-cd $HOME/project/src
+# cd $HOME/project/src
 
 bundle install
 
