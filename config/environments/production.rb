0bc40985f116508ba02b5ece51ce1d6eebc026a0
Justin Marsh <jmarsh24@gmail.com>
2022-06-20 18:11:53 +0200

    remove yt-dlp from build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,5 +107,5 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?w
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
