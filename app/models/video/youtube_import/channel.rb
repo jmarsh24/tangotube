3d95c13e63a71df5ce7db5774c2688e0345a10c1
Justin Marsh <jmarsh24@gmail.com>
2022-06-22 02:35:50 +0200

    call out full path in ruby files for yt-dlp

--- app/models/video/youtube_import/channel.rb
+++ app/models/video/youtube_import/channel.rb
@@ -1,6 +1,6 @@
 class Video::YoutubeImport::Channel
   YOUTUBE_DL_COMMAND_PREFIX =
-    "$HOME/project/src/yt-dlp https://www.youtube.com/channel/".freeze
+    "$HOME/project/src/yt-dlp/yt-dlp https://www.youtube.com/channel/".freeze
   YOUTUBE_DL_COMMAND_SUFFIX = "/videos  --get-id --skip-download".freeze
 
   class << self
