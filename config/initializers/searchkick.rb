21542bc2e2f47ac592d2d065ee38ecbff5893a08
Justin Marsh <jmarsh24@gmail.com>
2022-06-23 11:51:35 +0200

    increase searchkick timeout

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -2,5 +2,5 @@ Searchkick.client_options = {
   url: ENV["ELASTICSEARCH_URL"],
   retry_on_failure: true,
   transport_options:
-    { request: { timeout: 250 } }
+    { request: { timeout: 1000 } }
 }
