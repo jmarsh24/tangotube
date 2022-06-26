70fd8886eb6e7e4d8126e918d0cef1af168b8edf
Justin Marsh <jmarsh24@gmail.com>
2022-06-26 23:56:54 +0200

    Increase concurrency to 5

--- app/workers/update_video_worker.rb
+++ app/workers/update_video_worker.rb
@@ -5,7 +5,7 @@ class UpdateVideoWorker
 
   sidekiq_throttle(
     # Allow maximum 10 concurrent jobs of this class at a time.
-    concurrency: { limit: 3 },
+    concurrency: { limit: 5 },
     # Allow maximum 1K jobs being processed within one hour window.
     # threshold: { limit: 1_000, period: 1.hour }
   )
