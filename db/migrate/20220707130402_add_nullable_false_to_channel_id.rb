05b987cac9bb5328664d3a1d4d34105bd67d67c3
Justin Marsh <jmarsh24@gmail.com>
2022-07-07 15:30:55 +0200

    Add migrations for channel_id uniquness and not nullable

--- db/migrate/20220707130402_add_nullable_false_to_channel_id.rb
+++ db/migrate/20220707130402_add_nullable_false_to_channel_id.rb
@@ -1,4 +1,4 @@
-class AddUniquenessToChannelId < ActiveRecord::Migration[7.0]
+class AddNullableFalseToChannelId < ActiveRecord::Migration[7.0]
   def change
     change_column :channels, :channel_id, :string, null: false, unique: true
   end
