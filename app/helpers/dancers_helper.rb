9a80010fa249486c966399d376f835528ff466c4
Justin Marsh <jmarsh24@gmail.com>
2022-07-16 01:02:45 +0200

    Dancers working

--- app/helpers/dancers_helper.rb
+++ app/helpers/dancers_helper.rb
@@ -2,7 +2,7 @@ module DancersHelper
   def dancer_avatar(dancer_id)
     dancer = Dancer.find(dancer_id)
     if dancer.profile_image.attached?
-        image_tag dancer.profile_image
+        dancer.profile_image
     else
         image_url "default_avatar.jpg"
     end
