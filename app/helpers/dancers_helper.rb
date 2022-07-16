fb9d26d7f54b9aca18f0693689a1ef4634c4b9fa
Justin Marsh <jmarsh24@gmail.com>
2022-07-17 01:58:30 +0200

    Configure dancers view

--- app/helpers/dancers_helper.rb
+++ app/helpers/dancers_helper.rb
@@ -2,9 +2,18 @@ module DancersHelper
   def dancer_avatar(dancer_id)
     dancer = Dancer.find(dancer_id)
     if dancer.profile_image.attached?
-        dancer.profile_image
+      dancer.profile_image
     else
-        image_url "default_avatar.jpg"
+      image_url "default_avatar.jpg"
     end
-end
+  end
+
+  def dancer_cover(dancer_id)
+    dancer = Dancer.find(dancer_id)
+    if dancer.cover_image.attached?
+      dancer.cover_image
+    else
+      "blank_cover.jpg"
+    end
+  end
 end
