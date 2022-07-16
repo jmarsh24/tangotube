883ad9275bd784700c1c566c425fd81b3105bb4e
Justin Marsh <jmarsh24@gmail.com>
2022-07-16 10:41:09 +0200

    Add validation to dancervideos to prevent duplicates

--- app/models/dancer_video.rb
+++ app/models/dancer_video.rb
@@ -3,5 +3,6 @@ class DancerVideo < ApplicationRecord
   belongs_to :video
   counter_culture :dancer, column_name: "videos_count"
 
+  validates :dancer, uniqueness: { scope: :video }
   enum role: { neither: 0, leader: 1, follower: 2, both: 3 }
 end
