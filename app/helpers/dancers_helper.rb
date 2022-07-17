ae824f7894f979489d365ca45fc5f998af06a2c0
Justin Marsh <jmarsh24@gmail.com>
2022-07-17 10:08:44 +0200

    Add videos to dancer show page

--- app/helpers/dancers_helper.rb
+++ app/helpers/dancers_helper.rb
@@ -16,4 +16,22 @@ module DancersHelper
       "blank_cover.jpg"
     end
   end
+
+  def filtering_params
+    params.permit(
+      :leader,
+      :follower,
+      :channel,
+      :genre,
+      :orchestra,
+      :song,
+      :hd,
+      :event,
+      :year,
+      :watched,
+      :liked,
+      :id,
+      :query,
+      :dancer).to_h.flatten.join("_")
+  end
 end
