7b24210945add34ebf0403710354d21aa330bf92
Justin Marsh <jmarsh24@gmail.com>
2022-07-21 13:13:58 +0200

    Fix counter cache

--- app/models/video.rb
+++ app/models/video.rb
@@ -49,7 +49,7 @@ class Video < ApplicationRecord
   has_many :couples, through: :dancers
   has_one :orchestra, through: :song
   counter_culture :song
-  counter_culture [:orchestra, :song]
+  counter_culture [:song, :orchestra]
 
   scope :filter_by_orchestra, ->(song_artist, _user) { joins(:song).where("unaccent(songs.artist) ILIKE unaccent(?)", song_artist)}
   scope :filter_by_genre, ->(song_genre, _user) { joins(:song).where("unaccent(songs.genre) ILIKE unaccent(?)", song_genre) }
