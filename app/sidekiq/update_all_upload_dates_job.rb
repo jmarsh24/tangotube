34711ffb60aeb0d4150aa182a9da544dc637b2e1
Justin Marsh <jmarsh24@gmail.com>
2022-07-23 23:42:33 +0200

    Update uploaddates job

--- app/sidekiq/update_all_upload_dates_job.rb
+++ app/sidekiq/update_all_upload_dates_job.rb
@@ -4,11 +4,12 @@ class UpdateAllUploadDatesJob
   def perform(youtube_id)
     video = Video.find_by(youtube_id:)
     yt_video = Yt::Video.new id: video.youtube_id
-    video.upload_date = yt_video.published_at
     rescue Yt::Errors::NoItems => e
     if e.present?
       video.destroy
+    else
+      video.upload_date = yt_video.published_at
+      video.save
     end
-    video.save
   end
 end
