69fdb3f486c3a682775f16c7f580afb6d1b4463b
Justin Marsh <jmarsh24@gmail.com>
2022-07-23 23:32:02 +0200

    Add rescue for update_upload_dates job

--- app/sidekiq/update_all_upload_dates_job.rb
+++ app/sidekiq/update_all_upload_dates_job.rb
@@ -5,6 +5,10 @@ class UpdateAllUploadDatesJob
     video = Video.find_by(youtube_id:)
     yt_video = Yt::Video.new id: video.youtube_id
     video.upload_date = yt_video.published_at
+    rescue Yt::Errors::NoItems => e
+    if e.present?
+      video.destroy
+    end
     video.save
   end
 end
