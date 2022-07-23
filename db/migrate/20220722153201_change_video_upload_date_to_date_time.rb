1a6a221437897293b78f531086388b2d8c2c8ae9
Justin Marsh <jmarsh24@gmail.com>
2022-07-23 16:40:01 +0200

    Add missing migration

--- db/migrate/20220722153201_change_video_upload_date_to_date_time.rb
+++ db/migrate/20220722153201_change_video_upload_date_to_date_time.rb
@@ -2,7 +2,5 @@ class ChangeVideoUploadDateToDateTime < ActiveRecord::Migration[7.0]
   def change
     change_column :videos, :upload_date, :datetime
   end
-  Video.all.find_each do |video|
-    UpdateAllUploadDatesJob.perform_async(video.youtube_id)
-  end
+  CallUpdateAllVideosJob.perform_async
 end
