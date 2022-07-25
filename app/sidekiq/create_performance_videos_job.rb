b69776a96192f178890c9b1eaae6f9772c64ae74
Justin Marsh <jmarsh24@gmail.com>
2022-07-25 20:05:57 +0200

    Modify create performances job

--- app/sidekiq/create_performance_videos_job.rb
+++ app/sidekiq/create_performance_videos_job.rb
@@ -1,18 +1,28 @@
 class CreatePerformanceVideosJob
   include Sidekiq::Job
 
-  def perform(channel_id)
-    channel = Channel.find(channel_id)
-    channel.videos.group_by(&:upload_date).each do |upload_date, videos_with_same_upload_date|
-      if videos_with_same_upload_date.length > 1
+  def perform(video_id)
+    video = Video.find(video_id)
+    performance_videos =
+    Video.includes(:dancers, :performance)
+          .references(:dancers, :performance)
+          .where("upload_date <= ?", video.upload_date + 1.day)
+          .where("upload_date >= ?", video.upload_date - 1.day)
+          .where(dancers: { id: video.dancers.map(&:id) })
+          .order(performance_number: :asc)
+          .where(hidden: false)
+          .where(performance: { id: nil })
+
+      if performance_videos.length > 1
         performance = Performance.create
-        videos_with_same_upload_date.sort_by(&:upload_date).each_with_index do |video, index|
-          index = index + 1
-          PerformanceVideo.create(performance: performance, video: video, position: video&.performance_number || index )
-          performance.date = video&.performance_date
-          performance.save
-        end
+        performance_videos.each_with_index do |performance_video, index|
+        index += 1
+        PerformanceVideo.create(performance:, video: performance_video, position: video&.performance_number || index )
+        performance.date = video&.performance_date
+        performance.save
       end
     end
   end
 end
+
+
