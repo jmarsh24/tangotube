2e8aaef8aa2b0b45dd976abe5efb2157fc690cdb
Justin Marsh <jmarsh24@gmail.com>
2022-07-26 15:59:27 +0200

    Add video index job

--- app/sidekiq/my_sidekiq_job.rb
+++ app/sidekiq/my_sidekiq_job.rb
@@ -1,7 +1,15 @@
 class MySidekiqJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # Do something
+  def perform(id, remove)
+    if remove
+      # The record has likely already been removed from your database so we cannot
+      # use ActiveRecord#find to load it.
+      # We access the underlying Meilisearch index object.
+      Video.index.delete_document(id)
+    else
+      # The record should be present.
+      Video.find(id).index!
+    end
   end
 end
