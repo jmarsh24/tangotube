baa4fa7abc64c800ffea6a728812cd5c224220cd
Justin Marsh <jmarsh24@gmail.com>
2022-07-26 23:06:30 +0200

    Refactor meilisearch integration

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,6 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_host: Rails.env.production? ? "http://#{ENV["MEILISEARCH_URL"]}" : "http://localhost:7700",
-  meilisearch_api_key: Rails.env.production? ? ENV["MEILI_MASTER_KEY"] || "",
+  meilisearch_api_key: Rails.env.production? ? ENV["MEILI_MASTER_KEY"] : "",
   timeout: 2,
   max_retries: 1
 }
