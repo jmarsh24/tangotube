ffe35d62f187933094fd2b865a7b2c27b8db2323
Justin Marsh <jmarsh24@gmail.com>
2022-07-26 17:58:11 +0200

    Revert to index that runs out of ram

--- app/sidekiq/reindex_all_resources_job.rb
+++ app/sidekiq/reindex_all_resources_job.rb
@@ -7,8 +7,6 @@ class ReindexAllResourcesJob
     Song.reindex!
     Event.reindex!
     Channel.reindex!
-    Video.all.find_each do |video|
-      MySidekiqJob.perform_async(video.id, false)
-    end
+    Video.reindex!
   end
 end
