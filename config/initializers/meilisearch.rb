46880e36d3b3b1ad8b74b1f297b5afd63e2d4261
Justin Marsh <jmarsh24@gmail.com>
2022-07-26 14:24:55 +0200

    set to production with key

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,5 +1,6 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_host: "http://#{ENV["MEILISEARCH_URL"]}" || "http://localhost:7700",
+  meilisearch_api_key: ENV["MEILI_MASTER_KEY"] || "",
   timeout: 2,
   max_retries: 1
 }
