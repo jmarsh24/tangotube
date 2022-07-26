705bdf37a68444b594255ca686044ac879c10f3e
Justin Marsh <jmarsh24@gmail.com>
2022-07-26 14:04:10 +0200

    Remove master key

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,5 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_host: "http://#{ENV["MEILISEARCH_URL"]}" || "http://localhost:7700",
-  api_key: ENV["MEILI_MASTER_KEY"] || "",
   timeout: 2,
   max_retries: 1
 }
