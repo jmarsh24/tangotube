ca1f423c699d6f448381efd530963cb9d43a3590
Justin Marsh <jmarsh24@gmail.com>
2022-08-05 01:28:45 +0200

    remove http header from meilisearch

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,5 +1,5 @@
 MeiliSearch::Rails.configuration = {
-  meilisearch_host: Rails.env.production? ? "http://#{ENV["MEILISEARCH_URL"]}" : "http://localhost:7700",
+  meilisearch_host: Rails.env.production? ? "#{ENV["MEILISEARCH_URL"]}" : "http://localhost:7700",
   meilisearch_api_key: Rails.env.production? ? ENV["MEILI_MASTER_KEY"] : "",
   timeout: 10,
   max_retries: 1
