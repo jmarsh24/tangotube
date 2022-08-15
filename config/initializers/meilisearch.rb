0a99a4233ce70c8ee1809c3d6ee1a23316426fc0
Justin Marsh <jmarsh24@gmail.com>
2022-08-15 11:53:06 +0200

    Fix meilisearch http header

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,6 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_host: "http://#{ENV.fetch("MEILISEARCH_URL", '127.0.0.1:7700')}",
-  meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', nil),
+  meilisearch_api_key: ENV.fetch('MEILI_MASTER_KEY', nil),
   timeout: 10,
   max_retries: 1
 }
