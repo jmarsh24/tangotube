fdd6ca8a3230b716d5e931c7eec9bbcb636bf547
Justin Marsh <jmarsh24@gmail.com>
2022-08-22 20:23:27 +0200

    Fix autocomplete

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,23 +1,23 @@
 class SearchSuggestionsController < ApplicationController
-  # def search
-  #   @leaders = Leader.search(params[:query]).map(&:full_name)
+  def search
+    @leaders = Leader.where("name ILIKE ?", "%#{params[:query]}%").map(&:full_name)
 
-  #   @followers = Follower.search(params[:query]).map(&:full_name)
+    @followers = Follower.where("name ILIKE ?", "%#{params[:query]}%").map(&:full_name)
 
-  #   @songs = Song.search(params[:query]).map(&:full_title)
+    @songs = Song.where("title ILIKE ?", "%#{params[:query]}%").map(&:full_title)
 
-  #   @channels = Channel.search(params[:query]).map(&:title)
+    @channels = Channel.where("title ILIKE ?", "%#{params[:query]}%").map(&:title)
 
     @search_results = []
 
-  #     @search_results =
-  #       [@leaders + @followers + @songs + @channels].flatten
-  #         .uniq
-  #         .first(10)
-  #         .map(&:titleize)
+       @search_results =
+        [@leaders + @followers + @songs + @channels].flatten
+          .uniq
+          .first(10)
+          .map(&:titleize)
 
-  #   respond_to do |format|
-  #     format.turbo_stream
-  #   end
-  # end
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
 end
