231b588ab4e665d3de1b416948ab1903f2f8b21e
jmarsh <jmarsh24@gmail.com>
2022-09-02 16:00:13 +0200

    Add x86_64-linux to gem lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-server "your_ip_address", user: "deploy", roles: %w[app db web]
+set :application, "tangotube"
+set :repo_url, "git@github.com:jmarsh24/tangotube.git"
+server "142.93.101.60", user: "deploy", roles: %w[app db web]
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/tangotube"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
