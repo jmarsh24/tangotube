57fb990b8022d4188495092c8c60416e2c988fc7
jmarsh <jmarsh24@gmail.com>
2022-09-03 09:59:33 +0200

    Remove public file server enabled from application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options ={:host => 'tangotube.tv', :protocol => 'https'}
+  config.action_mailer.default_url_options ={host: "tangotube.tv", protocol: "https"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -96,16 +96,12 @@ Rails.application.configure do
 
   config.active_record.async_query_executor = :global_thread_pool
 
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL') { "redis://localhost:6379/1" } }
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
   config.session_store :cache_store,
-                      key: '_session',
+                      key: "_session",
                       compress: true,
                       pool_size: 5,
                       expire_after: 1.year
 
   config.action_view.image_loading = "lazy"
-
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
