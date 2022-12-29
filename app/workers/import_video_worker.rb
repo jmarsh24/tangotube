25d26ba73c98b7a335517ed74e8f6a428a507eb2
Justin Marsh <jmarsh24@gmail.com>
2022-12-29 21:24:37 +0100

    Add concurrency throttle to import video

--- app/workers/import_video_worker.rb
+++ app/workers/import_video_worker.rb
@@ -1,6 +1,11 @@
 class ImportVideoWorker
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
   sidekiq_options queue: :high, retry: 1
+
+  sidekiq_throttle(
+    concurrency: { limit: 5 }
+  )
 
   def perform(youtube_id)
     Video::YoutubeImport.from_video(youtube_id)
