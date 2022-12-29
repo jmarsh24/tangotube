7f9357a8290ca07b1bf61cb605e8b0680337af2c
Justin Marsh <jmarsh24@gmail.com>
2022-12-29 22:13:46 +0100

    Add concurrency to updatevideoworker

--- app/workers/update_video_worker.rb
+++ app/workers/update_video_worker.rb
@@ -1,6 +1,15 @@
 class UpdateVideoWorker
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
   sidekiq_options queue: :default, retry: 3
+
+  sidekiq_throttle(
+    # Allow maximum 10 concurrent jobs of this class at a time.
+    concurrency: { limit: 5 },
+    # Allow maximum 1K jobs being processed within one hour window.
+    # threshold: { limit: 1_000, period: 1.hour }
+  )
+
 
   def perform(youtube_id)
     Video::YoutubeImport::Video.update(youtube_id)
