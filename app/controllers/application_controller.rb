42619a7aecd1a00a18c66c5ceed221e73443e160
Justin Marsh <jmarsh24@gmail.com>
2023-01-13 15:03:00 +0100

    Add footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   newrelic_ignore_enduser
   include Pundit::Authorization
+  include Shimmer::Consent
   include Shimmer::RemoteNavigation
   before_action :total_videos_count
   before_action :configure_permitted_parameters, if: :devise_controller?
--- app/controllers/cookies_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CookiesController < ApplicationController
-  def index
-    session[:cookies_accepted] = params[:cookies_accepted] if params[:cookies_accepted]
-  end
-end
