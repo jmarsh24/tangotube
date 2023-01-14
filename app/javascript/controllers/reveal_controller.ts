4363dca199c1d543cf8509167aac3eb923bc85b4
Justin Marsh <jmarsh24@gmail.com>
2023-01-14 15:57:20 +0100

    Add reveal controller

--- app/javascript/controllers/reveal_controller.ts
+++ app/javascript/controllers/reveal_controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
-// Connects to data-controller="reveal"
 export default class extends Controller {
-  connect() {
+  hasHiddenClass: boolean;
+  hiddenClass: string;
+  itemTargets: HTMLElement[];
+  class: string;
+
+  static targets = ['item'];
+  static classes = ['hidden'];
+
+  connect(): void {
+    this.class = this.hasHiddenClass ? this.hiddenClass : 'hidden';
+  }
+
+  toggle(): void {
+    this.itemTargets.forEach((item) => {
+      item.classList.toggle(this.class);
+    });
+  }
+
+  show(): void {
+    this.itemTargets.forEach((item) => {
+      item.classList.remove(this.class);
+    });
+  }
+
+  hide(): void {
+    this.itemTargets.forEach((item) => {
+      item.classList.add(this.class);
+    });
   }
 }
