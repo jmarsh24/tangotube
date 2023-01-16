82c3e874c67c8548416d0d012ca962686e673a3d
Justin Marsh <jmarsh24@gmail.com>
2023-01-16 18:44:08 +0100

    remove missing javascript controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,85 +2,79 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application';
 
-import AutoclickController from "./autoclick_controller"
-application.register("autoclick", AutoclickController)
+import AutoclickController from './autoclick_controller';
+application.register('autoclick', AutoclickController);
 
-import AutosubmitController from "./autosubmit_controller"
-application.register("autosubmit", AutosubmitController)
+import AutosubmitController from './autosubmit_controller';
+application.register('autosubmit', AutosubmitController);
 
-import ClipboardController from "./clipboard_controller"
-application.register("clipboard", ClipboardController)
+import ClipboardController from './clipboard_controller';
+application.register('clipboard', ClipboardController);
 
-import CollapsibleController from "./collapsible_controller"
-application.register("collapsible", CollapsibleController)
+import CollapsibleController from './collapsible_controller';
+application.register('collapsible', CollapsibleController);
 
-import CollapsiblePanelController from "./collapsible_panel_controller"
-application.register("collapsible-panel", CollapsiblePanelController)
+import CollapsiblePanelController from './collapsible_panel_controller';
+application.register('collapsible-panel', CollapsiblePanelController);
 
-import ComboboxController from "./combobox_controller"
-application.register("combobox", ComboboxController)
+import ComboboxController from './combobox_controller';
+application.register('combobox', ComboboxController);
 
-import DarkModeController from "./dark_mode_controller"
-application.register("dark-mode", DarkModeController)
+import DropdownController from './dropdown_controller';
+application.register('dropdown', DropdownController);
 
-import DrawerController from "./drawer_controller"
-application.register("drawer", DrawerController)
+import FilterController from './filter_controller';
+application.register('filter', FilterController);
 
-import DropdownController from "./dropdown_controller"
-application.register("dropdown", DropdownController)
+import FlashController from './flash_controller';
+application.register('flash', FlashController);
 
-import FilterController from "./filter_controller"
-application.register("filter", FilterController)
+import FormController from './form_controller';
+application.register('form', FormController);
 
-import FlashController from "./flash_controller"
-application.register("flash", FlashController)
+import HideController from './hide_controller';
+application.register('hide', HideController);
 
-import FormController from "./form_controller"
-application.register("form", FormController)
+import ImageErrorController from './image_error_controller';
+application.register('image-error', ImageErrorController);
 
-import HideController from "./hide_controller"
-application.register("hide", HideController)
+import PopoverController from './popover_controller';
+application.register('popover', PopoverController);
 
-import ImageErrorController from "./image_error_controller"
-application.register("image-error", ImageErrorController)
+import PopperController from './popper_controller';
+application.register('popper', PopperController);
 
-import PopoverController from "./popover_controller"
-application.register("popover", PopoverController)
+import ReadMoreController from './read_more_controller';
+application.register('read-more', ReadMoreController);
 
-import PopperController from "./popper_controller"
-application.register("popper", PopperController)
+import ReplyController from './reply_controller';
+application.register('reply', ReplyController);
 
-import ReadMoreController from "./read_more_controller"
-application.register("read-more", ReadMoreController)
+import RevealController from './reveal_controller';
+application.register('reveal', RevealController);
 
-import ReplyController from "./reply_controller"
-application.register("reply", ReplyController)
+import SearchController from './search_controller';
+application.register('search', SearchController);
 
-import RevealController from "./reveal_controller"
-application.register("reveal", RevealController)
+import ShareController from './share_controller';
+application.register('share', ShareController);
 
-import SearchController from "./search_controller"
-application.register("search", SearchController)
+import TabsController from './tabs_controller';
+application.register('tabs', TabsController);
 
-import ShareController from "./share_controller"
-application.register("share", ShareController)
+import ThumbnailController from './thumbnail_controller';
+application.register('thumbnail', ThumbnailController);
 
-import TabsController from "./tabs_controller"
-application.register("tabs", TabsController)
+import ToggleController from './toggle_controller';
+application.register('toggle', ToggleController);
 
-import ThumbnailController from "./thumbnail_controller"
-application.register("thumbnail", ThumbnailController)
+import TomSelectController from './tom_select_controller';
+application.register('tom-select', TomSelectController);
 
-import ToggleController from "./toggle_controller"
-application.register("toggle", ToggleController)
+import TomSelectEditController from './tom_select_edit_controller';
+application.register('tom-select-edit', TomSelectEditController);
 
-import TomSelectController from "./tom_select_controller"
-application.register("tom-select", TomSelectController)
-
-import TomSelectEditController from "./tom_select_edit_controller"
-application.register("tom-select-edit", TomSelectEditController)
-
-import YoutubeController from "./youtube_controller"
-application.register("youtube", YoutubeController)
+import YoutubeController from './youtube_controller';
+application.register('youtube', YoutubeController);
