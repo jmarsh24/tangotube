ad30e0bdcb3d5b938ef50caa8bbe418c5470cd94
Justin Marsh <jmarsh24@gmail.com>
2023-01-19 15:42:52 +0100

    integrate propshaft and css bundling based on flink

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.assets.debug = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -73,4 +74,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.active_job.queue_adapter = :sidekiq
+  config.hotwire_livereload.reload_method = :turbo_stream
+  config.hosts = nil
 end
