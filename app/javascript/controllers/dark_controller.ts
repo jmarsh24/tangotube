bcb5b6ad2e545722b83e34c701f462ee65dbc76f
Justin Marsh <jmarsh24@gmail.com>
2023-01-30 15:31:54 +0100

    fully working dark mode selector

--- app/javascript/controllers/dark_controller.ts
+++ app/javascript/controllers/dark_controller.ts
@@ -1,68 +1,33 @@
 import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="dark"
+
+type theme = 'auto' | 'dark' | 'light';
 export default class extends Controller {
-  iconTarget!: HTMLElement;
-
-  static targets = ['icon'];
-
   connect() {
-    if (this.darkMode) {
-      this.dark();
-    } else {
-      this.light();
-    }
-
-    if (
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      this.setDarkIcon();
-    } else {
-      this.setLightIcon();
-    }
-  }
-
-  setLight() {
-    this.darkMode = true;
-    this.light();
-    this.setLightIcon();
-    this.light();
-  }
-
-  setDark() {
-    this.darkMode = false;
-    this.dark();
-    this.setDarkIcon();
+    document.body.dataset.theme = this.theme;
   }
 
   dark() {
-    document.body.classList.remove('light');
+    this.theme = 'dark';
+    document.body.dataset.theme = 'dark';
   }
 
   light() {
-    document.body.classList.add('light');
-  }
-
-  setDarkIcon() {
-    this.iconTarget.classList.remove('icon--sun-max');
-    this.iconTarget.classList.add('icon--moon-fill');
-  }
-
-  setLightIcon() {
-    this.iconTarget.classList.remove('icon--moon-fill');
-    this.iconTarget.classList.add('icon--sun-max');
+    this.theme = 'light';
+    document.body.dataset.theme = 'light';
   }
 
   auto() {
-    localStorage.removeItem('lightModeStatus');
+    this.theme = 'auto';
+    delete document.body.dataset['theme'];
   }
 
-  get darkMode(): boolean {
-    return localStorage.getItem('lightModeStatus') !== 'true';
+  get theme(): theme {
+    return (localStorage.getItem('theme') as theme) || 'auto';
   }
 
-  set darkMode(value: boolean) {
-    localStorage.setItem('lightModeStatus', value ? 'false' : 'true');
+  set theme(value: theme) {
+    localStorage.setItem('theme', value);
   }
 }
