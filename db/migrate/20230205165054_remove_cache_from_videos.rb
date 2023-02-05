0c0d06df569526fb6811084f9fad5d420581198e
Justin Marsh <jmarsh24@gmail.com>
2023-02-05 22:12:04 +0100

    fix migration for removing cache columns

--- db/migrate/20230205165054_remove_cache_from_videos.rb
+++ db/migrate/20230205165054_remove_cache_from_videos.rb
@@ -1,11 +1,11 @@
 class RemoveCacheFromVideos < ActiveRecord::Migration[7.0]
   def change
-    remove_column :videos, :cached_scoped_votes_total, :integer, default: 0
-    remove_column :videos, :cached_scoped_votes_score, :integer, default: 0
-    remove_column :videos, :cached_scoped_votes_up, :integer, default: 0
-    remove_column :videos, :cached_scoped_votes_down, :integer, default: 0
-    remove_column :videos, :cached_scoped_weighted_score, :integer, default: 0
-    remove_column :videos, :cached_scoped_weighted_total, :integer, default: 0
-    remove_column :videos, :cached_scoped_weighted_average, :float, default: 0.0
+    remove_column :videos, :cached_scoped_like_votes_total, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_votes_score, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_votes_up, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_votes_down, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_weighted_score, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_weighted_total, :integer, default: 0
+    remove_column :videos, :cached_scoped_like_weighted_average, :float, default: 0.0
   end
 end
