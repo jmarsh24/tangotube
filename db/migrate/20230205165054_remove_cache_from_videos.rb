74f7f616df2a765174ee4f43f807bcf6ceeeee54
Justin Marsh <jmarsh24@gmail.com>
2023-02-05 22:17:06 +0100

    fix migration for removing cache columns

--- db/migrate/20230205165054_remove_cache_from_videos.rb
+++ db/migrate/20230205165054_remove_cache_from_videos.rb
@@ -4,8 +4,8 @@ class RemoveCacheFromVideos < ActiveRecord::Migration[7.0]
     remove_column :videos, :cached_scoped_like_votes_score, :integer, default: 0
     remove_column :videos, :cached_scoped_like_votes_up, :integer, default: 0
     remove_column :videos, :cached_scoped_like_votes_down, :integer, default: 0
-    remove_column :videos, :cached_scoped_like_weighted_score, :integer, default: 0
-    remove_column :videos, :cached_scoped_like_weighted_total, :integer, default: 0
-    remove_column :videos, :cached_scoped_like_weighted_average, :float, default: 0.0
+    remove_column :videos, :cached_weighted_like_score, :integer, default: 0
+    remove_column :videos, :cached_weighted_like_total, :integer, default: 0
+    remove_column :videos, :cached_weighted_like_average, :float, default: 0.0
   end
 end
