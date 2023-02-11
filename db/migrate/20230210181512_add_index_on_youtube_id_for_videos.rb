ae9e803ed3b4592ecfa71ef8b16908b1fe365d15
Justin Marsh <jmarsh24@gmail.com>
2023-02-11 11:14:36 +0100

    try to remove index

--- db/migrate/20230210181512_add_index_on_youtube_id_for_videos.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexOnYoutubeIdForVideos < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :videos, :youtube_id
-    add_index :videos, :youtube_id, unique: true
-  end
-end
