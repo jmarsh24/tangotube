92074f35d9e2d84bd86a5d36c4afc21bb0a67516
Justin Marsh <jmarsh24@gmail.com>
2023-02-15 16:21:50 +0100

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_103047) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_095942) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
   enable_extension "unaccent"
