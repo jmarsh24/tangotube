cccacd0c65a0265a7bffd697318898556c544938
Justin Marsh <jmarsh24@gmail.com>
2023-02-17 19:55:58 +0100

    Enable rspotify

--- config/initializers/r_spotify.rb
+++ config/initializers/r_spotify.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-# if Rails.application.credentials.dig(:spotify, :client_id) && Rails.application.credentials.dig(:spotify, :secret_key)
-#   RSpotify.authenticate(Rails.application.credentials.dig(:spotify, :client_id), Rails.application.credentials.dig(:spotify, :secret_key))
-# end
+if Rails.application.credentials.dig(:spotify, :client_id) && Rails.application.credentials.dig(:spotify, :secret_key)
+  RSpotify.authenticate(Rails.application.credentials.dig(:spotify, :client_id), Rails.application.credentials.dig(:spotify, :secret_key))
+end
