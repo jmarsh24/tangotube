4ca9a7998bb440156f7f08dd57c6324c8e7374f3
Justin Marsh <jmarsh24@gmail.com>
2023-02-19 15:00:45 +0100

    Correct spotify secret key

--- config/initializers/r_spotify.rb
+++ config/initializers/r_spotify.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 if ENV["INTERNET"]
-  RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_client_secret!)
+  RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
 end
