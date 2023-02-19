999b40ac5ef812e9f0d87d434f4afacf4141ef35
Justin Marsh <jmarsh24@gmail.com>
2023-02-19 04:14:25 +0100

    system tests working

--- spec/support/system/cuprite_setup.rb
+++ spec/support/system/cuprite_setup.rb
@@ -13,8 +13,13 @@ Capybara.register_driver(:chrome) do |app|
       browser_options: {},
       # Increase Chrome startup wait time (required for stable CI builds)
       process_timeout: 10,
+      # Usually, especially when using Selenium, developers tend to increase the max wait time.
+      # With Cuprite, there is no need for that.
+      # We use a Capybara default value here explicitly.
+      default_max_wait_time: 2,
       # Enable debugging capabilities
       inspector: true,
+      js_errors: true,
       # Allow running Chrome in a headful mode by setting HEADLESS env
       # var to a falsey value
       headless: Config.headless?
@@ -22,6 +27,13 @@ Capybara.register_driver(:chrome) do |app|
   )
 end
 
+
 # Configure Capybara to use :cuprite driver by default
 Capybara.default_driver = Capybara.javascript_driver = :chrome
 Capybara.enable_aria_label = true
+# Normalize whitespaces when using `has_text?` and similar matchers,
+# i.e., ignore newlines, trailing spaces, etc.
+# That makes tests less dependent on slightly UI changes.
+Capybara.default_normalize_ws = true
+Capybara.disable_animation = true
+
