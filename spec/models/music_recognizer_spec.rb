4adaf0aa7b36100782e69518bd79fb899ddb6ae2
Justin Marsh <jmarsh24@gmail.com>
2023-02-19 15:19:56 +0100

    Working music_recognizer_spec

---
+++ spec/models/music_recognizer_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe MusicRecognizer do
+  fixtures :all
+  let(:sound_file) { file_fixture("audio_snippet.mp3") }
+
+  describe "recognize" do
+    it "returns the music data from ACR Cloud and Spotify", vcr: {preserve_exact_body_bytes: true} do
+      music_data = MusicRecognizer.process(sound_file:)
+      expected_response = JSON.parse file_fixture("music_recognizer_data.json").read
+      expect(music_data).to eq expected_response
+    end
+  end
+end
