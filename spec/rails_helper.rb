fd58c4c2c9216f920df4865d25a514438acd2eb5
Justin Marsh <jmarsh24@gmail.com>
2023-02-19 12:37:16 +0100

    Add all sensitive api keys to vcr placeholder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,11 +23,25 @@ VCR.configure do |config|
   config.ignore_localhost = true
   config.allow_http_connections_when_no_cassette = true
   config.configure_rspec_metadata!
-  config.define_cassette_placeholder("<SPOTIFY_CLIENT_ID>") { Config.spotify_client_id! }
-  config.define_cassette_placeholder("<SPOTIFY_SECRET_KEY>") { Config.spotify_secret_key! }
+  config.define_cassette_placeholder("<YOUTUBE_API_KEY>") { Config.youtube_api_key! }
+  config.define_cassette_placeholder("DEEPL_AUTH_KEY") { Config.deepl_auth_key! }
+  config.define_cassette_placeholder("<AVO_LICENSE_KEY>") { Config.avo_license_key! }
+  config.define_cassette_placeholder("<GIPHY_API_KEY>") { Config.giphy_api_key! }
   config.define_cassette_placeholder("<ACRCLOUD_ACCESS_KEY>") { Config.acr_cloud_access_key! }
   config.define_cassette_placeholder("<ACRCLOUD_SECRET_KEY>") { Config.acr_cloud_secret_key! }
-  config.define_cassette_placeholder("<YOUTUBE_API_KEY>") { Config.youtube_api_key! }
+  config.define_cassette_placeholder("<SPOTIFY_CLIENT_ID>") { Config.spotify_client_id! }
+  config.define_cassette_placeholder("<SPOTIFY_SECRET_KEY>") { Config.spotify_secret_key! }
+  config.define_cassette_placeholder("<SENDGRID_USERNAME>") { Config.sendgrid_user_name! }
+  config.define_cassette_placeholder("<SENDGRID_PASSWORD>") { Config.sendgrid_password! }
+  config.define_cassette_placeholder("<SENDGRID_API_KEY>") { Config.sendgrid_api_key! }
+  config.define_cassette_placeholder("<AWS_ACCESS_KEY_ID>") { Config.aws_access_key_id! }
+  config.define_cassette_placeholder("<AWS_ACCESS_ACCESS_KEY>") { Config.aws_access_access_key! }
+  config.define_cassette_placeholder("<GITHUB_KEY>") { Config.github_key! }
+  config.define_cassette_placeholder("<GITHUB_SECRET>") { Config.github_secret! }
+  config.define_cassette_placeholder("<GOOGLE_CLIENT_ID>") { Config.google_client_id! }
+  config.define_cassette_placeholder("<GOOGLE_CLIENT_SECRET>") { Config.google_client_secret! }
+  config.define_cassette_placeholder("<FACEBOOK_APP_ID>") { Config.facebook_app_id! }
+  config.define_cassette_placeholder("<FACEBOOK_APP_SECRET>") { Config.facebook_app_secret! }
 end
 
 RSpec.configure do |config|
