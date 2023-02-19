b38472852c8f327f027efa8afee2e789bd799703
Justin Marsh <jmarsh24@gmail.com>
2023-02-19 13:31:44 +0100

    Working acr_cloud spec

--- spec/models/acr_cloud_spec.rb
+++ spec/models/acr_cloud_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe AcrCloud do
   let(:sound_file) { file_fixture("audio.mp3") }
 
   describe "send" do
-    it "send a request to ACR Cloud" do
-      VCR.use_cassette("acr_cloud") do
-        AcrCloud.send(sound_file:).data
-      end
+    it "send a request to ACR Cloud", :vcr do
+      sound_data = AcrCloud.send(sound_file:).data
+      expected_response = JSON.parse file_fixture("acr_cloud_response.json").read
+      expect(sound_data).to eq expected_response
     end
   end
 end
