de62254850187ab5d50dc842f782de2d08a8bbb2
Justin Marsh <jmarsh24@gmail.com>
2023-02-20 21:27:57 +0100

    Fix youtube scraper spec

--- spec/models/youtube_scraper_spec.rb
+++ spec/models/youtube_scraper_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe YoutubeScraper do
   describe "scrape" do
     it "returns song artist and title if present", :vcr do
       metadata = YoutubeScraper.scrape(slug).metadata
-      expect(data).to eq ["Cuando El Amor Muere", "Carlos Di Sarli y su Orquesta Típica"]
+      expect(metadata).to eq ["Cuando El Amor Muere", "Carlos Di Sarli y su Orquesta Típica"]
     end
   end
 end
