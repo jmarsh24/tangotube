cebf6d31c98cc903cabc19eeee77803226ae5a5e
Justin Marsh <jmarsh24@gmail.com>
2023-02-20 15:32:35 +0100

    remove internet env variable

--- config/initializers/r_spotify.rb
+++ config/initializers/r_spotify.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
-if ENV["INTERNET"]
-  RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
-end
+RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
