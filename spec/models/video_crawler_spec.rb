2608f206f8ec8418ca34e7035e8064108e1acf9b
Justin Marsh <jmarsh24@gmail.com>
2023-02-21 02:28:42 +0100

    remove fit

--- spec/models/video_crawler_spec.rb
+++ spec/models/video_crawler_spec.rb
@@ -13,20 +13,20 @@ RSpec.describe VideoCrawler do
 
     it "returns the video data from youtube", vcr: {preserve_exact_body_bytes: true} do
       data = @data.dig(:youtube)
-      expect(@data.dig(:slug)).to eq "AQ9Ri3kWa_4"
-      expect(@data.dig(:title)).to eq "Noelia Hurtado & Carlitos Espinoza in Amsterdam 2014 #1"
-      expect(@data.dig(:description)).to eq "24-26.10.2014 r., Amsterdam, Netherlands,\nPerformance 25th Oct, \"Salon de los Sabados\" in Academia de Tango"
-      expect(@data.dig(:upload_date)).to eq "2014-10-26 15:21:29 UTC"
-      expect(@data.dig(:duration)).to eq 167
-      expect(@data.dig(:tags)).to eq ["Amsterdam", "Netherlands", "tango", "argentinian tango", "milonga", "noelia hurtado", "carlitos espinoza", "carlos espinoza", "espinoza", "hurtado", "noelia", "hurtado espinoza", "Salon de los Sabados", "Academia de Tango", "Nederland"]
-      expect(@data.dig(:hd)).to eq true
-      expect(@data.dig(:view_count)).to eq 1044
-      expect(@data.dig(:favorite_count)).to eq "2014-10-26 15:21:29 UTC"
-      expect(@data.dig(:like_count)).to eq 3
-      expect(@data.dig(:youtube_music)).eq ["Cuando El Amor Muere", "Carlos Di Sarli y su Orquesta Típica"]
+      expect(data.dig(:slug)).to eq "AQ9Ri3kWa_4"
+      expect(data.dig(:title)).to eq "Noelia Hurtado & Carlitos Espinoza in Amsterdam 2014 #1"
+      expect(data.dig(:description)).to eq "24-26.10.2014 r., Amsterdam, Netherlands,\nPerformance 25th Oct, \"Salon de los Sabados\" in Academia de Tango"
+      expect(data.dig(:upload_date)).to eq "2014-10-26 15:21:29 UTC"
+      expect(data.dig(:duration)).to eq 167
+      expect(data.dig(:tags)).to eq ["Amsterdam", "Netherlands", "tango", "argentinian tango", "milonga", "noelia hurtado", "carlitos espinoza", "carlos espinoza", "espinoza", "hurtado", "noelia", "hurtado espinoza", "Salon de los Sabados", "Academia de Tango", "Nederland"]
+      expect(data.dig(:hd)).to eq true
+      expect(data.dig(:view_count)).to eq 1044
+      expect(data.dig(:favorite_count)).to eq "2014-10-26 15:21:29 UTC"
+      expect(data.dig(:like_count)).to eq 3
+      expect(data.dig(:youtube_music)).eq ["Cuando El Amor Muere", "Carlos Di Sarli y su Orquesta Típica"]
     end
 
-    fit "returns the video data from acr cloud", vcr: {preserve_exact_body_bytes: true} do
+    it "returns the video data from acr cloud", vcr: {preserve_exact_body_bytes: true} do
       status = @data.dig :acrcloud, :status
       data = @data.dig :acrcloud, :metadata
       music = data.dig(:music)[0]
