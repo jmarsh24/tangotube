647b740754130dda32106a9839826bb93aa5d705
Justin Marsh <jmarsh24@gmail.com>
2023-02-21 10:13:33 +0100

    Fix spec to use symbolized keys

--- spec/models/acr_cloud_spec.rb
+++ spec/models/acr_cloud_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe AcrCloud do
   describe "send" do
     it "send a request to ACR Cloud", vcr: {preserve_exact_body_bytes: true} do
       sound_data = AcrCloud.send(audio_file:).data
-      expected_response = JSON.parse file_fixture("acr_cloud_response.json").read
+      expected_response = JSON.parse file_fixture("acr_cloud_response.json").read, symbolize_names: true
       expect(sound_data).to eq expected_response
     end
   end
