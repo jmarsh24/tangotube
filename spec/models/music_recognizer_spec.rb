be7f16b0c7cca9f786a056d958b8c96815c89d15
Justin Marsh <jmarsh24@gmail.com>
2023-02-28 16:11:05 +0100

    Fix music recognizer spec

--- spec/models/music_recognizer_spec.rb
+++ spec/models/music_recognizer_spec.rb
@@ -7,29 +7,29 @@ RSpec.describe MusicRecognizer do
   let(:slug) { videos(:video_1_featured).youtube_id }
 
   describe "recognize" do
-    xit "returns the music data from ACR Cloud and Spotify", vcr: {preserve_exact_body_bytes: true} do
+    it "returns the music data from ACR Cloud and Spotify", vcr: {preserve_exact_body_bytes: true} do
       # allow(AudioDownloader).to receive(:filepath).and_return(file_fixture("audio.mp3").to_path)
       # allow(AudioProcessor).to receive(:filepath).and_return(file_fixture("audio_snippet.mp3").to_path)
       @data = MusicRecognizer.new.process_audio_snippet(slug)
-      status = @data.dig :acrcloud, :status
-      data = @data.dig :acrcloud, :metadata
-      music = data.dig(:music)[0]
+      status = @data.dig :status
+      metadata = @data.dig :metadata
+      music = metadata.dig(:music)[0]
       expect(status.dig(:code)).to eq 0
       expect(status.dig(:msg)).to eq "Success"
-      expect(music.dig(:title)).to eq "Cuando El Amor Muere"
-      expect(music.dig(:album, :name)).to eq "Serie 78 RPM : Carlos Di Sarli Vol.2"
-      expect(music.dig(:acrid)).to eq "a8d9899317fd427b6741b739de8ded15"
-      expect(music.dig(:external_ids, :isrc)).to eq "ARF034100046"
-      expect(music.dig(:artists, 0, :name)).to eq "Carlos Di Sarli y su Orquesta Típica"
-      expect(music.dig(:album, :name)).to eq "Serie 78 RPM : Carlos Di Sarli Vol.2"
+      expect(music.dig(:title)).to eq "La Mentirosa"
+      expect(music.dig(:album, :name)).to eq "Cantan Alberto Morán Y Roberto Chanel"
+      expect(music.dig(:acrid)).to eq "0d07891de1a0b282efce9b20dfce2bba"
+      expect(music.dig(:external_ids, :isrc)).to eq "ARF040200415"
+      expect(music.dig(:artists, 0, :name)).to eq "Osvaldo Pugliese"
+      expect(music.dig(:artists, 1, :name)).to eq "Alberto Moran"
+      expect(music.dig(:album, :name)).to eq "Cantan Alberto Morán Y Roberto Chanel"
       expect(music.dig(:genres, 0, :name)).to eq "Tango"
-      expect(music.dig(:external_metadata, :spotify, :artists, 0, :name)).to eq "Carlos Acuña"
-      expect(music.dig(:external_metadata, :spotify, :artists, 1, :name)).to eq "Carlos Di Sarli"
-      expect(music.dig(:external_metadata, :spotify, :track, :name)).to eq "Cuando El Amor Muere"
-      expect(music.dig(:external_metadata, :spotify, :track, :id)).to eq "66jpnblQkPOngiFwEEyUW3"
-      expect(music.dig(:external_metadata, :spotify, :album, :name)).to eq "Classics (1940-1943)"
-      expect(music.dig(:external_metadata, :youtube, :vid)).to eq "p0AQ3gx3eo8"
-      expect(File.exist?("/tmp/audio/video_#{slug}/#{slug}_snippet.mp3")).to be false
+      expect(music.dig(:external_metadata, :spotify, :artists, 0, :name)).to eq "Osvaldo Pugliese"
+      expect(music.dig(:external_metadata, :spotify, :artists, 1, :name)).to eq "Alberto Moran"
+      expect(music.dig(:external_metadata, :spotify, :track, :name)).to eq "La Mentirosa"
+      expect(music.dig(:external_metadata, :spotify, :track, :id)).to eq "0iAj9eP9ZjNw7QcvjRoxgO"
+      expect(music.dig(:external_metadata, :spotify, :album, :name)).to eq "Canta garganta con arena"
+      expect(music.dig(:external_metadata, :youtube, :vid)).to eq "9Yj-xPNOMo8"
     end
   end
 end
