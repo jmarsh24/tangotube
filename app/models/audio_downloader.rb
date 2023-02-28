a1e6412fd738d4fbc990bd2841f2bfc9b9bef171
Justin Marsh <jmarsh24@gmail.com>
2023-02-28 06:34:24 +0100

    Change audio_downloader to yield file

--- app/models/audio_downloader.rb
+++ app/models/audio_downloader.rb
@@ -2,21 +2,25 @@ class AudioDownloader
   YT_DLP_COMMAND_PREFIX = " https://www.youtube.com/watch?v=".freeze
   YT_DLP_COMMAND_DOWNLOAD_AUDIO = " -f 140 --force-overwrites -o ".freeze
 
-  attr_reader :filepath
-
   def initialize(slug)
     @slug = slug
     @filepath = Rails.root.join("tmp/audio/video_#{slug}/#{slug}.mp3")
   end
 
   def self.download(slug:)
-    new(slug).tap(&:download)
+    new(slug).download
   end
 
   def download
-    system(ENV["YT_DLP_BIN"] + YT_DLP_COMMAND_PREFIX + @slug + YT_DLP_COMMAND_DOWNLOAD_AUDIO + @filepath.to_s)
-  rescue => e
-    Rails.logger.warn "AudioDownloader yt-dlp download error: #{e.backtrace.join("\n\t")}"
-    ""
+    Tempfile.create(@slug.to_s) do |file|
+      system(yt_dlp_command)
+    rescue => e
+      Rails.logger.warn "AudioDownloader yt-dlp download error: #{e.backtrace.join("\n\t")}"
+      ""
+    end
+  end
+
+  def yt_dlp_command
+    ENV["YT_DLP_BIN"] + YT_DLP_COMMAND_PREFIX + @slug + YT_DLP_COMMAND_DOWNLOAD_AUDIO + file.path
   end
 end
--- spec/models/audio_downloader_spec.rb
+++ spec/models/audio_downloader_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe AudioDownloader do
   let(:slug) { videos(:video_1_featured).youtube_id }
 
   describe "download" do
-    it "returns the video data from youtube and acrcloud" do
-      audio_filepath = AudioDownloader.download(slug:).filepath
-      expected_filepath = Rails.root.join("tmp/audio/video_#{slug}/#{slug}.mp3").to_s
-      expect(audio_filepath.to_s).to eq expected_filepath
-      expect(File.size(audio_filepath)).to eq 2688467
-      expect(File.extname(audio_filepath)).to eq ".mp3"
+    fit "returns the video data from youtube and acrcloud" do
+      file = file_fixture("audio.mp3").open
+      AudioDownloader.download(slug:) do |downloaded_file|
+        expect(downloaded_file.read).to eq file.read
+      end
     end
   end
 end
