bbb3f331082699fc5d00ffebf76c0c19ca9d2532
Justin Marsh <jmarsh24@gmail.com>
2023-02-28 08:23:59 +0100

    Convert audio_processor to audio_trimmer

--- app/models/audio_downloader.rb
+++ app/models/audio_downloader.rb
@@ -8,20 +8,21 @@ class AudioDownloader
   end
 
   def self.download(slug:)
-    new(slug).with_download_file
+    new(slug).with_download_file do |file|
+      yield file
+    end
   end
 
   def with_download_file
     Tempfile.create(@slug.to_s) do |file|
-      system(yt_dlp_command)
-      yield file
+      system(yt_dlp_command(file))
+      file
     rescue => e
       Rails.logger.warn "AudioDownloader yt-dlp download error: #{e.backtrace.join("\n\t")}"
-      ""
     end
   end
 
-  def yt_dlp_command
+  def yt_dlp_command(file)
     ENV["YT_DLP_BIN"] + YT_DLP_COMMAND_PREFIX + @slug + YT_DLP_COMMAND_DOWNLOAD_AUDIO + file.path
   end
 end
