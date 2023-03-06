959789244120f427fb940997976c977560fd9766
Justin Marsh <jmarsh24@gmail.com>
2023-03-06 11:29:18 +0100

    remove env_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@
 
 require "devise"
 require "spec_helper"
-require "env_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
