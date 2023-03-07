0f52a21a6127a7025dd2db6b1da65449f062cb7c
Justin Marsh <jmarsh24@gmail.com>
2023-03-07 20:13:20 +0100

    fix specs

--- spec/models/audio_trimmer_spec.rb
+++ spec/models/audio_trimmer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe AudioTrimmer do
 
   describe "download" do
     it "returns the video data from youtube and acrcloud" do
-      audio_file = file_fixture("audio.mp3")
+      audio_file = file_fixture("audio.mp3").open
       AudioTrimmer.new.trim(audio_file) do |trimmed_file|
         expect(trimmed_file.read).to eq file_fixture("audio_snippet.mp3").read
         expect(File.size(trimmed_file)).to eq 320926
