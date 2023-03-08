407cc8ed3577114a682eb7f0ab17abd63dd0752f
Justin Marsh <jmarsh24@gmail.com>
2023-03-08 12:58:02 +0100

    add ensure block

--- spec/models/youtube_audio_downloader_spec.rb
+++ spec/models/youtube_audio_downloader_spec.rb
@@ -8,9 +8,13 @@ RSpec.describe YoutubeAudioDownloader do
 
   describe "download_file" do
     it "returns the video data from youtube and acrcloud" do
-      file = file_fixture("blank_audio.mp3").open
+      audio_file = Tempfile.new
+      audio_file.binmode
+      audio_file.write file_fixture("audio.mp3").read
+      audio_file.rewind
+
       YoutubeAudioDownloader.new.download_file(slug) do |downloaded_file|
-        expect(downloaded_file.read).to eq file.read
+        expect(downloaded_file.read).to eq audio_file.read
       end
     end
   end
