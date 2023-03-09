cfeca765b1cd1dc9e2ae5f50fd2c6126ddc9f0c7
Justin Marsh <jmarsh24@gmail.com>
2023-03-09 10:28:32 +0100

    Working youtube audio downloader

--- spec/models/youtube_audio_downloader_spec.rb
+++ spec/models/youtube_audio_downloader_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe YoutubeAudioDownloader do
     it "returns the video data from youtube and acrcloud" do
       audio_file = Tempfile.new
       audio_file.binmode
-      audio_file.write file_fixture("audio.mp3").read
+      audio_file.write file_fixture("blank_audio.mp3").read
       audio_file.rewind
 
-      YoutubeAudioDownloader.new.download_file(slug) do |downloaded_file|
-        expect(downloaded_file.read).to eq audio_file.read
-      end
+      downloaded_file = YoutubeAudioDownloader.new.download_file(slug)
+      expect(downloaded_file.read).to eq audio_file.read
     end
   end
 end
