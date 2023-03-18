39602da831815fee7d2f8d8d792f1a131383efa2
Justin Marsh <jmarsh24@gmail.com>
2023-03-18 15:02:31 +0100

    refactored tests

--- spec/models/youtube_audio_downloader_spec.rb
+++ spec/models/youtube_audio_downloader_spec.rb
@@ -5,14 +5,10 @@ require "rails_helper"
 RSpec.describe YoutubeAudioDownloader do
   fixtures :all
   let(:slug) { videos(:video_1_featured).youtube_id }
+  let(:audio_file) { file_fixture("blank_audio.mp3") }
 
   describe "#download_file" do
     it "returns the video data from youtube and acrcloud" do
-      audio_file = Tempfile.new
-      audio_file.binmode
-      audio_file.write file_fixture("blank_audio.mp3").read
-      audio_file.rewind
-
       YoutubeAudioDownloader.new.download_file(slug) do |downloaded_file|
         expect(downloaded_file.read).to eq audio_file.read
       end
