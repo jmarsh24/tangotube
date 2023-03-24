b7d7a497bc95d62cf7af7e1f74f7bd8d54c69bd8
Justin Marsh <jmarsh24@gmail.com>
2023-03-24 19:53:41 +0100

    working external_video_importer

--- app/models/video_metadata.rb
+++ app/models/video_metadata.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
-VideoMetadata =
-  Struct.new(
-    :youtube,
-    :acr_cloud,
-    keyword_init: true
-  )
+VideoMetadata = Struct.new(:youtube, :acr_cloud, keyword_init: true) do
+  def song_attributes
+    [youtube.song.titles.join(","),
+      youtube.song.artist,
+      youtube.song.album,
+      acr_cloud.acr_song_title,
+      acr_cloud.acr_artist_names.join(","),
+      acr_cloud.acr_album_name,
+      acr_cloud.spotify_artist_names.join(","),
+      acr_cloud.spotify_track_name,
+      acr_cloud.spotify_album_name].join(",")
+  end
+end
