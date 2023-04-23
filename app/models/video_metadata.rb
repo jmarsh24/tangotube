1e5d412beb724a329cc43073824e6fb0c312419e
Justin Marsh <jmarsh24@gmail.com>
2023-04-23 16:11:17 +0200

    merge

--- app/models/video_metadata.rb
+++ app/models/video_metadata.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
-VideoMetadata = Struct.new(:youtube, :acr_cloud, keyword_init: true) do
-  def song_attributes
-    [youtube.song.titles.join(","),
-      youtube.song.artist,
-      youtube.song.album,
-      acr_cloud.acr_song_title,
-      acr_cloud.acr_artist_names.join(","),
-      acr_cloud.acr_album_name,
-      acr_cloud.spotify_artist_names.join(","),
-      acr_cloud.spotify_track_name,
-      acr_cloud.spotify_album_name].join(",")
-  end
-end
+VideoMetadata =
+  Struct.new(
+    :youtube,
+    :acr_cloud,
+    keyword_init: true
+  )
