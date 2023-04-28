ae4eb084aafd5541e98a364215d7ca2191627900
Justin Marsh <jmarsh24@gmail.com>
2023-04-28 18:56:43 +0200

    Working basic songmatcher test

--- app/models/external_video_import/metadata_processing/songMatcher.rb
+++ app/models/external_video_import/metadata_processing/songMatcher.rb
@@ -1,14 +1,14 @@
 module ExternalVideoImport
   module MetadataProcessing
     class SongMatcher
-      def match(video_metadata)
+      def match(artist_fields:, title_fields:, metadata_fields:)
         songs = ::Song.all.pluck(:id, :title, :last_name_search)
-        text = [video_metadata.music.song_field, video_metadata.music.artist_field].join(" ")
-        song_data = Trigram.best_match(list: songs, text:) do |song|
+        text = [metadata_fields, artist_fields, title_fields].join(" ")
+        song_data = Trigram.best_matches(list: songs, text:) do |song|
           [song[1], song[2]].join(" ")
         end
         if song_data
-          ::Song.find(song_data[0])
+          ::Song.find(song_data.first[0])
         else
           ::Song.create(title: video_metadata.music.song_field, artist_name: video_metadata.music.artist_field)
         end
--- app/models/external_video_import/metadata_processing/trigram.rb
+++ app/models/external_video_import/metadata_processing/trigram.rb
@@ -4,27 +4,27 @@ module ExternalVideoImport
   module MetadataProcessing
     class Trigram
       class << self
-        def best_match(list:, text:, threshold: 0.3, &block)
+        def best_matches(list:, text:, threshold: 0.3, &block)
           list.map do |item|
-            search_string = block.call(item)
-            similarity_ratio = inclusion_similarity(search_string, text)
+            query = block.call(item)
+            similarity_ratio = inclusion_similarity(query, text)
             [item, similarity_ratio]
           end.select { |_, similarity| similarity >= threshold }.max_by { |_, similarity| similarity }
         end
 
         private
 
-        def inclusion_similarity(text1, text2)
-          text1_trigrams = trigramify(text1)
-          text2_trigrams = trigramify(text2)
+        def inclusion_similarity(item_text, query_text)
+          item_text_trigrams = trigramify(item_text)
+          query_text_trigrams = trigramify(query_text)
 
-          same_size = (text1_trigrams & text2_trigrams).size
+          same_size = (item_text_trigrams & query_text_trigrams).size
 
-          same_size.to_f / text1_trigrams.size
+          same_size.to_f / item_text_trigrams.size
         end
 
         def trigramify(text)
-          text.parameterize(" ").chars.each_cons(3).map(&:join)
+          text.parameterize(separator: " ").chars.each_cons(3).map(&:join)
         end
       end
     end
