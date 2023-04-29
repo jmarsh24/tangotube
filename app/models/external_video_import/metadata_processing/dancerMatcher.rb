b268c134a594062aab90d53e33a332fbbca3ac10
Justin Marsh <jmarsh24@gmail.com>
2023-04-29 13:08:52 +0200

    Working event matcher

--- app/models/external_video_import/metadata_processing/dancerMatcher.rb
+++ app/models/external_video_import/metadata_processing/dancerMatcher.rb
@@ -3,16 +3,22 @@ module ExternalVideoImport
     class DancerMatcher
       def match(metadata_fields:)
         dancers = ::Dancer.all.pluck(:id, :first_name, :last_name)
-        text = [metadata_fields].join " "
-        dancer_data = Trigram.best_matches(list: dancers, text: text, threshold: 0.4) do |dancer|
-          [dancer[1], dancer[2], dancer[3]].join " "
-        end
+        text = metadata_fields.join(" ")
+        dancer_ids = find_best_matches(dancers, text, threshold: 0.4)
 
-        if dancer_data.any?
-          ::Dancer.find(dancer_data.map(&:first).map(&:first))
+        if dancer_ids.any?
+          ::Dancer.find(dancer_ids)
         else
           []
         end
+      end
+
+      private
+
+      def find_best_matches(dancers, text, threshold:)
+        Trigram.best_matches(list: dancers, text: text, threshold: threshold) do |dancer|
+          [dancer[1], dancer[2], dancer[3]].join(" ")
+        end.map { |match| match.first.first }
       end
     end
   end
