1d61f5d3cb439ff93894e02e5c2c8d3072b5b8b6
Justin Marsh <jmarsh24@gmail.com>
2023-04-29 19:45:06 +0200

    tests for trigram spec

--- spec/models/external_video_import/metadata_processing/trigram_spec.rb
+++ spec/models/external_video_import/metadata_processing/trigram_spec.rb
@@ -3,14 +3,37 @@
 require "rails_helper"
 
 RSpec.describe ExternalVideoImport::MetadataProcessing::Trigram do
-  # test with special characters
-  # describe "#best_match" do
-  #   it "computes the similarity" do
-  #     expect(ExternalVideoImport::MetadataProcessing::Trigram.best_match(list: [1, "he is genius", "test"], text: "he is very genius")).to eq 0.5625
+  let(:items) { ["Pepito Avellaneda & Tete Rusconi", "Carlos Gavito & Marcela Durán", "Geraldine Rojas & Ezequiel Paludi"] }
+  let(:text) { "Pepito Avellaneda y Tete Rusconi - Tango" }
+  let(:threshold) { 0.6 }
 
-  #     expect(ExternalVideoImport::MetadataProcessing::Trigram.best_match(list: [1, "he is genius", "test"], text: "I can fly")).to eq 0
+  describe "#best_matches" do
+    it "returns the best matches for the given list" do
+      best_matches = described_class.best_matches(list: items, text: text, threshold: threshold) { |item| item }
 
-  #     expect(ExternalVideoImport::MetadataProcessing::Trigram.best_match(list: [1, "he is genius", "test"], text: "he is genius")).to eq 1
-  #   end
-  # end
+      expect(best_matches).to eq([[items[0], 0.9642857142857143]])
+    end
+
+    it "returns an empty array when no match is found" do
+      text = "Random dancers - Tango"
+      best_matches = described_class.best_matches(list: items, text: text, threshold: threshold) { |item| item }
+
+      expect(best_matches).to eq([])
+    end
+
+    it "returns a match for partial names" do
+      text = "Pepito & Tete - Tango"
+      best_matches = described_class.best_matches(list: items, text: text, threshold: threshold) { |item| item }
+
+      expect(best_matches).to eq([[items[0], 0.625]])
+    end
+
+    it "returns a match for accented characters" do
+      items_with_accents = ["Martín Maldonado & Maurizio Ghella", "Esteban Cortez & Evelyn Rivera", "Gustavo Naveira & Giselle Anne"]
+      text = "Martín Maldonado y Maurizio Ghella - Tango"
+      best_matches = described_class.best_matches(list: items_with_accents, text: text, threshold: threshold) { |item| item }
+
+      expect(best_matches).to eq([[items_with_accents[0], 0.9333333333333333]])
+    end
+  end
 end
