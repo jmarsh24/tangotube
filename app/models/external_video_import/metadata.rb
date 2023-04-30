b3f0eb167487a330903175b9938ded597ea56dce
Justin Marsh <jmarsh24@gmail.com>
2023-04-30 16:22:13 +0200

    flatten and compact the arrays

--- app/models/external_video_import/metadata.rb
+++ app/models/external_video_import/metadata.rb
@@ -11,7 +11,7 @@ module ExternalVideoImport
           music.acr_song_title,
           music.spotify_track_name,
           youtube.song.titles
-        ]
+        ].flatten.compact
       end
 
       def searchable_artist_names
@@ -19,7 +19,7 @@ module ExternalVideoImport
           music.spotify_artist_names,
           music.acr_artist_names,
           youtube.song.artist
-        ]
+        ].flatten.compact
       end
 
       def searchable_music_fields
@@ -28,12 +28,12 @@ module ExternalVideoImport
           music.spotify_album_name,
           youtube.song&.album,
           youtube.song&.writers
-        ]
+        ].flatten.compact
       end
 
       def genre_fields
         external_genres = RSpotify::Track.find(music.spotify_track_id).artists.first.genres
-        [music.genre, external_genres]
+        [music.genre, external_genres].flatten.compact
       end
     end
   end
