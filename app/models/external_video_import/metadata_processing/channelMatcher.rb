63c517587c15d63f199881c6dbfc100b1773b542
Justin Marsh <jmarsh24@gmail.com>
2023-04-30 16:33:52 +0200

    Working channel spec

--- app/models/external_video_import/metadata_processing/channelMatcher.rb
+++ app/models/external_video_import/metadata_processing/channelMatcher.rb
@@ -16,7 +16,13 @@ module ExternalVideoImport
       end
 
       def create_channel(channel_metadata)
-        channel = ::Channel.create!(channel_id: channel_metadata.id)
+        channel = ::Channel.create!(
+          channel_id: channel_metadata.id,
+          title: channel_metadata.title,
+          description: channel_metadata.description,
+          thumbnail_url: channel_metadata.thumbnail_url,
+          video_count: channel_metadata.video_count
+        )
         attach_avatar_thumbnail(channel, channel_metadata.thumbnail_url)
         channel
       end
