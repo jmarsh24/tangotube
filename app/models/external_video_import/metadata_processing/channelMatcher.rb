daea9ebb99d151100d0067472b1e46f6c0903e90
Justin Marsh <jmarsh24@gmail.com>
2023-04-30 16:02:56 +0200

    refactored

--- app/models/external_video_import/metadata_processing/channelMatcher.rb
+++ app/models/external_video_import/metadata_processing/channelMatcher.rb
@@ -1,8 +1,10 @@
-# frozen_string_literal: true
-
 module ExternalVideoImport
   module MetadataProcessing
     class ChannelMatcher
+      def initialize(thumbnail_attacher = ThumbnailAttacher)
+        @thumbnail_attacher = thumbnail_attacher
+      end
+
       def match_or_create(channel_metadata:)
         match_channel(channel_metadata) || create_channel(channel_metadata)
       end
@@ -14,7 +16,15 @@ module ExternalVideoImport
       end
 
       def create_channel(channel_metadata)
-        ::Channel.create!(channel_id: channel_metadata.id)
+        channel = ::Channel.create!(channel_id: channel_metadata.id)
+        attach_avatar_thumbnail(channel, channel_metadata.thumbnail_url)
+        channel
+      end
+
+      def attach_avatar_thumbnail(channel, thumbnail_url)
+        return if thumbnail_url.blank?
+
+        @thumbnail_attacher.attach_thumbnail(channel, thumbnail_url)
       end
     end
   end
--- spec/models/external_video_import/thumbnail_attacher_spec.rb
+++ spec/models/external_video_import/thumbnail_attacher_spec.rb
@@ -1,44 +1,33 @@
 require "rails_helper"
 
 RSpec.describe ExternalVideoImport::MetadataProcessing::ThumbnailAttacher do
-  let(:video) { instance_double("Video") }
+  let(:object) { double("Object", thumbnail: double("Thumbnail")) }
   let(:thumbnail_url) { "https://example.com/thumbnail.jpg" }
 
-  before do
-    allow(video).to receive(:youtube_id).and_return("test_video_slug")
-  end
-
-  describe ".attach_thumbnail" do
+  describe "#attach_thumbnail" do
     context "when thumbnail URL is not blank" do
-      it "attaches the thumbnail to the video" do
-        expect(Down).to receive(:download).with(thumbnail_url, destination: instance_of(String)).and_return(true)
-
-        thumbnail_instance = instance_double("Thumbnail")
-        allow(video).to receive(:thumbnail).and_return(thumbnail_instance)
+      it "attaches the thumbnail to the object" do
+        allow(object).to receive(:class).and_return(Object)
 
         tempfile = Tempfile.new(["thumbnail", ".jpg"])
-        allow(Tempfile).to receive(:open).and_yield(tempfile)
+        expect(Tempfile).to receive(:open).and_yield(tempfile)
 
-        expect(thumbnail_instance).to receive(:attach).with(
+        expect(Down).to receive(:download).with(thumbnail_url, destination: anything).and_return(true)
+        expect(object.thumbnail).to receive(:attach).with(
           io: tempfile,
-          filename: "#{video.youtube_id}.jpg"
+          filename: "object_#{thumbnail_url}.jpg"
         )
 
-        described_class.attach_thumbnail(video, thumbnail_url)
+        described_class.new.attach_thumbnail(object, thumbnail_url)
       end
     end
 
     context "when thumbnail URL is blank" do
-      it "does not attach the thumbnail to the video" do
+      it "does not attach the thumbnail to the object" do
         expect(Down).not_to receive(:download)
-        thumbnail_instance = instance_double("Thumbnail")
-        allow(video).to receive(:thumbnail).and_return(thumbnail_instance)
+        expect(object.thumbnail).not_to receive(:attach)
 
-        expect do
-          described_class.attach_thumbnail(video, "")
-        end.not_to change { video.thumbnail }
-
-        expect(thumbnail_instance).not_to receive(:attach)
+        described_class.new.attach_thumbnail(object, "")
       end
     end
 
@@ -50,16 +39,16 @@ RSpec.describe ExternalVideoImport::MetadataProcessing::ThumbnailAttacher do
       end
 
       it "logs a warning and does not attach the thumbnail" do
-        expect(Down).to receive(:download).with(thumbnail_url, destination: anything).and_raise(Down::Error, "Failed to download thumbnail")
-        thumbnail_instance = instance_double("Thumbnail")
-        allow(video).to receive(:thumbnail).and_return(thumbnail_instance)
+        allow(Down).to receive(:download).with(thumbnail_url, destination: anything).and_raise(Down::Error, "Failed to download thumbnail")
+        allow(Rails.logger).to receive(:warn)
 
-        expect(Rails.logger).to receive(:warn).with(/Failed to download thumbnail/)
-        expect(video.thumbnail).not_to receive(:attach)
+        expect(object.thumbnail).not_to receive(:attach)
 
         expect do
-          described_class.attach_thumbnail(video, thumbnail_url)
-        end.not_to change { video.thumbnail }
+          described_class.new.attach_thumbnail(object, thumbnail_url)
+        end.not_to change { object.thumbnail }
+
+        expect(Rails.logger).to have_received(:warn).with(/Failed to download thumbnail/)
       end
     end
   end
