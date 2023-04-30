13d4abdeb0135ee46562b2052b253d177d59c206
Justin Marsh <jmarsh24@gmail.com>
2023-04-30 16:03:16 +0200

    change namespace of musicmetadata object

---
+++ app/models/external_video_import/music_recognition/metadata.rb
@@ -0,0 +1,24 @@
+require "store_model"
+
+module ExternalVideoImport
+  module MusicRecognition
+    class Metadata
+      include StoreModel::Model
+
+      attribute :code, :integer
+      attribute :message, :string
+      attribute :acr_song_title, :string
+      attribute :acr_artist_names, :array_of_strings, default: -> { [] }
+      attribute :acr_album_name, :string
+      attribute :acr_id, :string
+      attribute :isrc, :string
+      attribute :genre, :string
+      attribute :spotify_artist_names, :array_of_strings, default: -> { [] }
+      attribute :spotify_track_name, :string
+      attribute :spotify_track_id, :string
+      attribute :spotify_album_name, :string
+      attribute :spotify_album_id, :string
+      attribute :youtube_vid, :string
+    end
+  end
+end
