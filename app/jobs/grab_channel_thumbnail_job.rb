856edbd2be9cf6fd4b59ab2c1100887d972b473d
Justin Marsh <jmarsh24@gmail.com>
2023-05-01 18:30:06 +0200

    remove unused jobs

--- app/jobs/grab_channel_thumbnail_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class GrabChannelThumbnailJob < ApplicationJob
-  queue_as :low_priority
-
-  def perform(video)
-    channel.grab_thumbnail
-  end
-end
--- app/jobs/grep_dancer_names_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class GrepDancerNamesJob < ApplicationJob
-  queue_as :low_priority
-
-  def perform
-    Dancer.all.find_each(&:find_videos)
-  end
-end
