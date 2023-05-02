4a65197e0e5c27dbcd63468952882b8fed13f2eb
Justin Marsh <jmarsh24@gmail.com>
2023-05-02 13:39:24 +0200

    scale workers

--- app/models/external_video_import/youtube/scraper.rb
+++ app/models/external_video_import/youtube/scraper.rb
@@ -14,7 +14,7 @@ module ExternalVideoImport
       YOUTUBE_URL_PREFIX = "https://www.youtube.com/watch?v="
       RETRY_COUNT = 100
 
-      def initialize(driver: Capybara::Cuprite::Driver.new(app: nil, browser_options: {headless: true, timeout: 120}))
+      def initialize(driver: Capybara::Cuprite::Driver.new(nil, {headless: true, timeout: 5.minutes}))
         @driver = driver
       end
 
