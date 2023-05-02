a6c63050ef2dafbdf2f3c42a9d0d0879c59822cc
Justin Marsh <jmarsh24@gmail.com>
2023-05-02 22:33:35 +0200

    remove memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"], {username: ENV["MEMCACHIER_USERNAME"], password: ENV["MEMCACHIER_PASSWORD"]}
+  config.cache_store = :null_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
