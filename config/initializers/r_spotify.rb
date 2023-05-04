397143235c8e7ba19207f36b698f3262a8d92d35
Justin Marsh <jmarsh24@gmail.com>
2023-05-04 14:25:53 +0200

    comment out all config

--- config/initializers/r_spotify.rb
+++ config/initializers/r_spotify.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 unless Rails.env.test?
-  RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
+  # RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
 end
