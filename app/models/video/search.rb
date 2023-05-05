73d3b80c0f87c377e16d2adafc0346a6163bbd99
Justin Marsh <jmarsh24@gmail.com>
2023-05-05 03:19:04 +0200

    fix tessts

--- app/models/video/search.rb
+++ app/models/video/search.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-class Video::Search
-  SEARCHABLE_COLUMNS = [
-    "songs.title",
-    "songs.last_name_search",
-    "channels.title",
-    "performance_videos.performance_id",
-    "videos.channel_title",
-    "videos.performance_date",
-    "videos.view_count",
-    "videos.updated_at",
-    "videos.popularity",
-    "videos.like_count",
-    "videos.upload_date"
-  ].freeze
+SEARCHABLE_COLUMNS = [
+  "songs.title",
+  "songs.last_name_search",
+  "channels.title",
+  "performance_videos.performance_id",
+  "videos.channel_title",
+  "videos.performance_date",
+  "videos.view_count",
+  "videos.updated_at",
+  "videos.popularity",
+  "videos.like_count",
+  "videos.upload_date"
+].freeze
 
+class Video::Search
   def initialize(filtering_params: {}, sorting_params: {}, user: nil)
     @filtering_params = filtering_params
     @sorting_params = sorting_params
@@ -22,11 +22,7 @@ class Video::Search
   end
 
   def self.for(filtering_params:, sorting_params:, user:)
-    new(
-      filtering_params:,
-      sorting_params:,
-      user:
-    )
+    new(filtering_params: filtering_params, sorting_params: sorting_params, user: user)
   end
 
   def videos
@@ -35,28 +31,34 @@ class Video::Search
       .filter_by(@filtering_params, @user)
   end
 
-  def leaders
-    @leaders ||= facet("dancers.name", {dancer_videos: :dancer}, role: :leader)
+  def dancer_leaders
+    @dancer_leaders ||= facet("dancers.name", {dancer_videos: :dancer}, role: :leader)
   end
 
-  def followers
-    @followers ||= facet("dancers.name", {dancer_videos: :dancer}, role: :follower)
+  def dancer_followers
+    @dancer_followers ||= facet("dancers.name", {dancer_videos: :dancer}, role: :follower)
   end
 
-  def orchestras
-    @orchestras ||= facet("songs.artist", :song)
+  def song_orchestras
+    @song_orchestras ||= facet("songs.artist", :song)
   end
 
-  def genres
-    @genres ||= facet("songs.genre", :song)
+  def song_genres
+    @song_genres ||= facet("songs.genre", :song)
   end
 
-  def years
-    @years ||= facet_on_year("performance_date")
+  def performance_years
+    @performance_years ||= facet_on_year("performance_date")
   end
 
-  def songs
-    @songs ||= facet("songs.title", :song)
+  def song_titles
+    @song_titles ||= facet("songs.title", :song)
+  end
+
+  private
+
+  def ordering_params
+    "#{sort_column} #{sort_direction}"
   end
 
   def sort_column
@@ -67,28 +69,15 @@ class Video::Search
     ["asc", "desc"].include?(@sorting_params[:direction]) ? @sorting_params[:direction] : "desc"
   end
 
-  def filter_column
-    @filtering_params
-  end
-
-  private
-
-  def ordering_params
-    (@filtering_params.empty? && @sorting_params.empty?) ? "RANDOM()" : "#{sort_column} #{sort_direction}"
-  end
-
   def facet_on_year(table_column)
-    query =
-      "extract(year from #{table_column})::int AS facet_value, count(#{table_column}) AS occurrences"
-    counts =
-      Video
-        .filter_by(@filtering_params, @user)
-        .not_hidden
-        .select(query)
-        .group("facet_value")
-        .order("facet_value DESC")
-        .having("count(#{table_column}) > 0")
-        .load_async
+    query = "extract(year from #{table_column})::int AS facet_value, count(#{table_column}) AS occurrences"
+    counts = Video.filter_by(@filtering_params, @user)
+      .not_hidden
+      .select(query)
+      .group("facet_value")
+      .order("facet_value DESC")
+      .having("count(#{table_column}) > 0")
+      .load_async
     counts.map { |c| ["#{c.facet_value} (#{c.occurrences})", c.facet_value] }
   end
 
@@ -97,33 +86,14 @@ class Video::Search
     videos = Video.filter_by(@filtering_params, @user)
       .not_hidden
       .joins(model)
-    videos = videos.merge(Video.where(dancer_videos: {role:})) if role.present?
-    counts = videos
-      .select(query)
+    videos = videos.merge(Video.where(dancer_videos: {role: role})) if role.present?
+    counts = videos.select(query)
       .group(table_column)
       .order("occurrences DESC")
       .having("count(#{table_column}) > 0")
       .load_async
     counts.map do |c|
       ["#{c.facet_value.split("'").map(&:titleize).join("'")} (#{c.occurrences})", c.facet_value.downcase]
-    end
-  end
-
-  def facet_id(table_column, table_column_id, model)
-    query =
-      "#{table_column} AS facet_value, count(#{table_column}) AS occurrences, #{table_column_id} AS facet_id_value"
-    counts =
-      Video
-        .filter_by(@filtering_params, @user)
-        .not_hidden
-        .joins(model)
-        .select(query)
-        .group(table_column, table_column_id)
-        .order("occurrences DESC")
-        .having("count(#{table_column}) > 0")
-        .load_async
-    counts.map do |c|
-      ["#{c.facet_value.titleize} (#{c.occurrences})", c.facet_id_value]
     end
   end
 end
