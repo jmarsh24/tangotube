1f3ea44ba441259403d1eb8e6d337b9253da1649
Justin Marsh <jmarsh24@gmail.com>
2023-05-05 15:43:52 +0200

    Fix intializers and dockerfile

--- config/initializers/r_spotify.rb
+++ config/initializers/r_spotify.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-unless Rails.env.test?
+if !Rails.env.test? && Config.spotify_client_id? && Config.spotify_secret_key?
   RSpotify.authenticate(Config.spotify_client_id!, Config.spotify_secret_key!)
 end
