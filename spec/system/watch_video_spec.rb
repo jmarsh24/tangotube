f6e70d7a2c05d96c718f5b30619a11c7f1a695fb
Justin Marsh <jmarsh24@gmail.com>
2023-05-06 13:07:08 +0200

    add test for watch video

---
+++ spec/system/watch_video_spec.rb
@@ -0,0 +1,12 @@
+require "system_helper"
+
+RSpec.describe "watch_video", type: :system do
+  fixtures :all
+
+  it "displays the video and related information" do
+    video = videos(:video_1_featured)
+    visit watch_path(v: video.youtube_id)
+    expect(page).to have_content(video.display.dancer_names)
+    expect(page).to have_content(video.display.any_song_attributes)
+  end
+end
