ce6a40983324c4f6f985ba2eac4053055595735f
Justin Marsh <jmarsh24@gmail.com>
2023-05-07 15:30:36 +0200

    rubocop

--- app/models/concerns/presentable.rb
+++ app/models/concerns/presentable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Presentable
   def decorate
     "#{self.class}Presenter".constantize.new(self)
--- app/presenters/video_presenter.rb
+++ app/presenters/video_presenter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class VideoPresenter < SimpleDelegator
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::UrlHelper
