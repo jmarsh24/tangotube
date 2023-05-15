333fe6be56aa7768b83d4ab6df424a8c7969086c
Justin Marsh <jmarsh24@gmail.com>
2023-05-15 11:09:58 +0200

    rubocop

---
+++ db/migrate/20230515081935_remove_gender_and_rename_gender_new_in_dancers.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RemoveGenderAndRenameGenderNewInDancers < ActiveRecord::Migration[7.1]
+  def up
+    remove_column :dancers, :gender
+    rename_column :dancers, :gender_new, :gender
+  end
+
+  def down
+    add_column :dancers, :gender_new, :gender_new
+    execute <<-SQL
+      UPDATE dancers
+      SET gender_new = CASE gender
+        WHEN 'male' THEN 0
+        WHEN 'female' THEN 1
+      END
+    SQL
+    remove_column :dancers, :gender
+    rename_column :dancers, :gender_new, :gender
+  end
+end
