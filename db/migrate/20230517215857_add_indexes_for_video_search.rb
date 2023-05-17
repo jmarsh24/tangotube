694778756f1c70ade58dc640f0ade6b5cf30e90d
Justin Marsh <jmarsh24@gmail.com>
2023-05-18 00:08:35 +0200

    add migrations

---
+++ db/migrate/20230517215857_add_indexes_for_video_search.rb
@@ -0,0 +1,8 @@
+class AddIndexesForVideoSearch < ActiveRecord::Migration[7.1]
+  def change
+    add_index :videos, :upload_date
+    add_index :videos, :click_count
+    add_index :dancers, :slug
+    add_index :orchestras, :slug
+  end
+end
