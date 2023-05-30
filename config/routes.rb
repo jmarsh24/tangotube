ca0c4d3a72981b8da7c59ec8bb0c8eb0a0ffb00c
Justin Marsh <jmarsh24@gmail.com>
2023-05-30 11:16:40 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,82 +1,47 @@
-# frozen_string_literal: true
-
-require "sidekiq/web"
-require "sidekiq-scheduler/web"
-
 Rails.application.routes.draw do
+  # Health checks
   get "up" => "rails/health#show", :as => :rails_health_check
-
+  get "status", to: "system_status#show"
+  
+  # Error routes
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 
-  authenticate :user, ->(user) { user.admin? } do
-    mount Avo::Engine, at: Avo.configuration.root_path
-  end
-  get "sitemaps/*path", to: "shimmer/sitemaps#show"
-  get "status", to: "system_status#show"
-  get "banner", to: "banner#index"
-  post "banner", to: "banner#index"
-
+  # User Authentication
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
 
-  resources :deletion_requests, only: [:show] do
-    collection do
-      post :facebook
+  # Admin section
+  authenticate :user, ->(user) { user.admin? } do
+    mount Avo::Engine, at: Avo.configuration.root_path
+    mount Sidekiq::Web, at: "sidekiq", as: :sidekiq
+    resources :channels do
+      post "deactivate", to: "channels#deactivate"
     end
   end
-
-  devise_scope :user do
-    authenticate :user, ->(u) { u.admin? } do
-      mount Sidekiq::Web, at: "sidekiq", as: :sidekiq
-      resources :channels do
-        post "deactivate", to: "channels#deactivate"
-      end
-    end
-  end
-
-  resources :comments do
-    resources :comments, module: :comments
-  end
-
-  resources :playlists, only: [:index, :create]
-  resources :clips do
-    collection do
-      post :index
-    end
-  end
-  resources :couples do
-    collection do
-      post :index
-    end
+  
+  # Public pages
+  get "/privacy", to: "pages#privacy"
+  get "/terms", to: "pages#terms"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  
+  # Main resources
+  resources :dancers, :events, :songs, only: [:index, :show] do
     member do
       post :show
     end
-  end
-  resources :orchestras do
-    collection do
-      post :index
-    end
-    member do
-      post :show
-    end
-  end
-  resources :performances do
     collection do
       post :index
     end
   end
-  resource :manifest, only: :show
-  resources :videos 
-    resource :filters, only: [:show]
-    resources :clips
-    collection do
-      get "filters", to: "filters#filters"
-    end
-    resources :comments, module: :videos
+
+  resources :videos do
+    resource :filter, only: [:show], controller: 'videos/filters' 
+    resources :clips, :comments, module: :videos
     member do
       patch "upvote", to: "videos#upvote"
       patch "downvote", to: "videos#downvote"
@@ -88,42 +53,35 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :webhooks
-
-  resources :search_suggestions, only: :index do
+  resources :playlists, :clips, :couples, :orchestras, :performances, only: [:index, :show, :create] do
+    member do
+      post :show
+    end
+    collection do
+      post :index
+    end
+  end
+  
+  resources :webhooks, :search_suggestions, only: [:index] do
     collection do
       post :search
     end
   end
 
-  resources :dancers do
-    member do
-      post :show
-    end
-  end
-  resources :events do
+  resources :deletion_requests, only: [:show] do
     collection do
-      post :index
-    end
-    member do
-      post :show
-    end
-  end
-  resources :songs do
-    collection do
-      post :index
-    end
-    member do
-      post :show
+      post :facebook
     end
   end
 
+  # Other routes
+  get "sitemaps/*path", to: "shimmer/sitemaps#show"
+  get "banner", to: "banner#index"
+  post "banner", to: "banner#index"
   post "savenew", to: "users#savenew"
   post "/" => "videos#index"
   get "/watch", to: "videos#show"
-  get "/privacy", to: "pages#privacy"
-  get "/terms", to: "pages#terms"
-  get "/about", to: "pages#about"
-  get "/contact", to: "pages#contact"
+
+  # Root
   root "videos#index"
 end
