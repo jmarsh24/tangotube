c1be225d4fefffef535a2ef122e780859c2fa786
Justin Marsh <jmarsh24@gmail.com>
2023-06-02 10:14:17 +0200

    fix word_similarity

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -32,7 +32,7 @@ module Indexable
       Array.wrap(terms)
         .map { |e| e.tr("*", "").downcase }
         .reduce(self) do |scope, term|
-          scope.where("similarity(index, ?) > 0.02", "%#{term}%")
+          scope.where("word_similarity(?, index) > .3", "%#{term}%")
         end
     end
     def index!(now: false)
