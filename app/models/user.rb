af8a840a26558ebb70d5df030a7410b0882e5029
Justin Marsh <jmarsh24@gmail.com>
2023-06-10 19:12:47 +0200

    Fix user log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,14 +63,15 @@ class User < ApplicationRecord
 
   class << self
     def from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
-        user.name = auth.info.name
+        user.name = auth.info.name   # assuming the user model has a name
         user.first_name = auth.info.first_name
         user.last_name = auth.info.last_name
         user.image = auth.info.image
-        user.provider = auth.info.provider
+        user.provider = auth.provider
+        user.skip_confirmation!
       end
     end
   end
