a751ae125b89820869ad14c4c010590748538fa0
Justin Marsh <jmarsh24@gmail.com>
2023-06-13 01:48:16 +0200

    remove unused models and disable scraper and music recognizer on main import

--- app/jobs/import_new_videos_for_all_channels_job.rb
+++ app/jobs/import_new_videos_for_all_channels_job.rb
@@ -4,6 +4,6 @@ class ImportNewVideosForAllChannelsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Channel.active.find_each(&:import_new_videos)
+    Channel.active.find_each(&:import_new_videos(use_scraper: false, use_music_recognizer: false))
   end
 end
