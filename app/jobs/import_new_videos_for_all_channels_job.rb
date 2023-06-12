8ca46daf4a0855b1c23c8347de390d984d0ab253
Justin Marsh <jmarsh24@gmail.com>
2023-06-13 01:49:17 +0200

    fix

--- app/jobs/import_new_videos_for_all_channels_job.rb
+++ app/jobs/import_new_videos_for_all_channels_job.rb
@@ -4,6 +4,8 @@ class ImportNewVideosForAllChannelsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Channel.active.find_each(&:import_new_videos(use_scraper: false, use_music_recognizer: false))
+    Channel.active.find_each do |channel|
+      channel.import_new_videos(use_scraper: false, use_music_recognizer: false)
+    end
   end
 end
