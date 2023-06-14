4e1e92e21c2efc29ed402c5fc2bcfe8aad41854b
Justin Marsh <jmarsh24@gmail.com>
2023-06-14 23:39:53 +0200

    fix

--- app/models/playlist_video.rb
+++ app/models/playlist_video.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: playlist_videos
+#
+#  id          :bigint           not null, primary key
+#  playlist_id :bigint           not null
+#  video_id    :bigint           not null
+#  position    :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class PlaylistVideo < ApplicationRecord
   belongs_to :playlist
   belongs_to :video
--- spec/jobs/index_video_job_spec.rb
+++ spec/jobs/index_video_job_spec.rb
@@ -2,6 +2,6 @@
 
 require "rails_helper"
 
-RSpec.describe IndexVideoJob, type: :job do
+RSpec.describe IndexVideosJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
