596b126bb796f9f3555e87d97f5b32ed1c7b7d74
Justin Marsh <jmarsh24@gmail.com>
2023-06-16 17:44:14 +0200

    Facet builder finished

--- app/models/video/facet_builder.rb
+++ app/models/video/facet_builder.rb
@@ -1,5 +1,6 @@
 class Video::FacetBuilder
-  Facet = Struct.new(:name, :id, :count, keyword_init: true)
+  Facet = Struct.new(:name, :options, keyword_init: true)
+  Option = Struct.new(:value, :label, keyword_init: true)
 
   def initialize(video_relation)
     @video_relation = video_relation
@@ -7,59 +8,90 @@ class Video::FacetBuilder
 
   def leaders
     facet_data = Dancer.joins(:dancer_videos)
-      .where(dancer_videos: {role: "leader", video_id: @video_relation.pluck(:id)})
-      .pluck("dancers.name, dancers.slug")
+      .where(dancer_videos: {role: "leader", video_id: @video_relation.select(:id)})
+      .group("dancers.name", "dancers.slug")
+      .order("count_all DESC", "dancers.name")
+      .count
+    facet = Facet.new(name: "Leaders", options: [])
+    facet.options = facet_data.map do |(name, value), count|
+      Option.new(label: "#{name} (#{count})", value:)
+    end
 
-    facet_data.tally.map do |(name, id), count|
-      Facet.new(name: "#{name} (#{count})", id:, count:)
-    end.sort_by { |facet| [-facet.count, facet.name] }
+    facet
   end
 
   def followers
     facet_data = Dancer.joins(:dancer_videos)
-      .where(dancer_videos: {role: "follower", video_id: @video_relation.pluck(:id)})
-      .pluck("dancers.name, dancers.slug")
+      .where(dancer_videos: {role: "follower", video_id: @video_relation.select(:id)})
+      .group("dancers.name", "dancers.slug")
+      .order("count_all DESC", "dancers.name")
+      .count
 
-    facet_data.tally.map do |(name, id), count|
-      Facet.new(name: "#{name} (#{count})", id:, count:)
-    end.sort_by { |facet| [-facet.count, facet.name] }
+    facet = Facet.new(name: "Followers", options: [])
+    facet.options = facet_data.map do |(name, value), count|
+      Option.new(label: "#{name} (#{count})", value:)
+    end
+
+    facet
   end
 
   def orchestras
     facet_data = @video_relation
       .joins(song: :orchestra)
-      .pluck("orchestras.name, orchestras.slug")
+      .group("orchestras.name", "orchestras.slug")
+      .order("count_all DESC", "orchestras.name")
+      .count
 
-    facet_data.tally.map do |(name, id), count|
-      Facet.new(name: "#{name} (#{count})", id:, count:)
-    end.sort_by { |facet| [-facet.count, facet.name] }
+    facet = Facet.new(name: "Orchestras", options: [])
+    facet.options = facet_data.map do |(name, value), count|
+      Option.new(label: "#{name.titleize} (#{count})", value:)
+    end
+
+    facet
   end
 
   def genres
     facet_data = @video_relation
       .joins(:song)
-      .pluck("songs.genre")
+      .group("LOWER(songs.genre)")
+      .order("count_all DESC", "LOWER(songs.genre)")
+      .count
 
-    facet_data.tally.map do |genre, count|
-      Facet.new(name: "#{genre} (#{count})", id: genre.downcase, count:)
-    end.sort_by { |facet| [-facet.count, facet.name] }
+    facet = Facet.new(name: "Genres", options: [])
+    facet.options = facet_data.map do |(name, value), count|
+      Option.new(label: "#{name.titleize} (#{count})", value:)
+    end
+
+    facet
   end
 
   def years
-    facet_data = @video_relation.pluck(:upload_date_year)
+    facet_data = @video_relation
+      .group(:upload_date_year)
+      .order("count_all DESC", upload_date_year: :desc)
+      .count
 
-    facet_data.tally.map do |year, count|
-      Facet.new(name: "#{year} (#{count})", id: year, count:)
-    end.sort_by { |facet| [-facet.count, -facet.name] }
+    facet = Facet.new(name: "Years", options: [])
+
+    facet.options = facet_data.map do |value, count|
+      Option.new(label: "#{value} (#{count})", value:)
+    end
+
+    facet
   end
 
   def songs
     facet_data = @video_relation
       .joins(:song)
-      .pluck("songs.title, songs.slug")
+      .group("songs.title", "songs.slug")
+      .order("count_all DESC", "songs.title")
+      .count
 
-    facet_data.tally.map do |(name, id), count|
-      Facet.new(name: "#{name} (#{count})", id:, count:)
-    end.sort_by { |facet| [-facet.count, facet.name] }
+    facet = Facet.new(name: "Songs", options: [])
+    facet.options = facet_data.map do |(name, value), count|
+      Option.new(label: "#{name} (#{count})", value:)
+    end
+
+    facet
   end
 end
