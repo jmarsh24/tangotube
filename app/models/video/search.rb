56c7e48fe131b0b13740db8d5c04bb52269875a5
Justin Marsh <jmarsh24@gmail.com>
2023-06-16 17:50:27 +0200

    refactored search.rb

--- app/models/video/search.rb
+++ app/models/video/search.rb
@@ -2,9 +2,7 @@ class Video::Search
   attr_reader :filtering_params, :sorting_params
   attr_accessor :current_user
 
-  Facets = Struct.new(:leaders, :followers, :orchestras, :genres, :years, :songs, keyword_init: true)
-
-  def initialize(filtering_params: {hidden: false}, sorting_params: {sort: "popularity", direction: "desc"}, hidden: false, current_user: nil)
+  def initialize(filtering_params: {hidden: false}, sorting_params: {sort: "popularity", direction: "desc"}, hidden: false, user: nil)
     @filtering_params = filtering_params
     @sorting_params = sorting_params
     @current_user = current_user
@@ -12,25 +10,16 @@ class Video::Search
   end
 
   def videos
-    filtered_videos = Video::Filter.new(Video.all, filtering_params:, current_user:).apply_filter
-    Video::Sort.new(filtered_videos, sorting_params:).apply_sort
+    filtered_videos = Video::Filter.new(Video.all, filtering_params:, current_user:).filtered_videos
+    Video::Sort.new(filtered_videos, sorting_params:).sorted_videos
   end
 
   def featured_videos
     Video.where(featured: true).order("RANDOM()")
   end
 
-  def facets
-    videos = Video::Filter.new(Video.all, filtering_params:, current_user:).apply_filter
-    facet_builder = Video::FacetBuilder.new(videos)
-
-    Facets.new(
-      leaders: facet_builder.leaders,
-      followers: facet_builder.followers,
-      orchestras: facet_builder.orchestras,
-      genres: facet_builder.genres,
-      years: facet_builder.years,
-      songs: facet_builder.songs
-    )
+  def facet(name:)
+    videos = Video::Filter.new(Video.all, filtering_params:, current_user:).filtered_videos
+    Video::FacetBuilder.new(videos).public_send(name)
   end
 end
