b6690dfe87dce7352d5eb4d425f6b8690bdbd5af
Justin Marsh <jmarsh24@gmail.com>
2023-06-20 19:43:33 +0200

    Add link to spotify and youtube

--- app/javascript/controllers/supported_controller.ts
+++ app/javascript/controllers/supported_controller.ts
@@ -1,7 +1,44 @@
 import { Controller } from '@hotwired/stimulus';
 
-export default class extends Controller {
+export default class SupportedController extends Controller {
   connect(): void {
-    this.element.classList.toggle('share-supported', 'canShare' in navigator);
+    this.toggleShareSupport();
+    this.toggleYoutubeAppSupport();
+    this.toggleSpotifyAppSupport();
+  }
+
+  toggleShareSupport(): void {
+    const isShareSupported = 'canShare' in navigator;
+    this.element.classList.toggle('share-supported', isShareSupported);
+  }
+
+  toggleYoutubeAppSupport(): void {
+    const isYoutubeAppSupported = this.isYoutubeAppSupported();
+    this.element.classList.toggle(
+      'youtube-app-supported',
+      isYoutubeAppSupported
+    );
+  }
+
+  toggleSpotifyAppSupport(): void {
+    const isSpotifyAppSupported = this.isSpotifyAppSupported();
+    this.element.classList.toggle(
+      'spotify-app-supported',
+      isSpotifyAppSupported
+    );
+  }
+
+  isYoutubeAppSupported(): boolean {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return (
+      (userAgent.includes('android') && userAgent.includes('chrome')) ||
+      userAgent.includes('iphone') ||
+      userAgent.includes('ipad')
+    );
+  }
+
+  isSpotifyAppSupported(): boolean {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return userAgent.includes('spotify');
   }
 }
