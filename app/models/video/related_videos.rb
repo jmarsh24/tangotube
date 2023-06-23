75f4950551ad2b7cbd397b902e55ddd9c98977b8
Justin Marsh <jmarsh24@gmail.com>
2023-06-23 22:11:04 +0200

    recommended videos refactor

--- app/models/video/related_videos.rb
+++ app/models/video/related_videos.rb
@@ -5,38 +5,38 @@ class Video::RelatedVideos
 
   def initialize(video)
     @video = video
+    @videos = Video.includes(Video.search_includes)
   end
 
   def with_same_dancers
-    videos = Video.exclude_youtube_id(@video.youtube_id)
     leader = @video.leaders.first.slug
     follower = @video.followers.first.slug
-    Video::Filter.new(videos.includes(Video.search_includes), filtering_params: {leader:, follower:, hidden: false}).filtered_videos
+    Video::Filter.new(@videos, filtering_params: {leader:, follower:, hidden: false}).filtered_videos
   end
 
   def with_same_event
-    videos = Video.within_week_of(@video.upload_date).exclude_youtube_id(@video.youtube_id)
+    videos = @videos.within_week_of(@video.upload_date)
     event = @video.event.slug
-    Video::Filter.new(videos.includes(Video.search_includes), filtering_params: {event:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {event:, hidden: false}).filtered_videos
   end
 
   def with_same_song
-    videos = Video.has_leader.has_follower.exclude_youtube_id(@video.youtube_id)
+    videos = @videos.has_leader.has_follower
     song = @video.song.slug
-    Video::Filter.new(videos.includes(Video.search_includes), filtering_params: {song:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {song:, hidden: false}).filtered_videos
   end
 
   def with_same_channel
-    videos = Video.has_leader.has_follower.exclude_youtube_id(@video.youtube_id)
+    videos = @videos.has_leader.has_follower
     channel = @video.channel.channel_id
-    Video::Filter.new(videos.includes(Video.search_includes), filtering_params: {channel:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {channel:, hidden: false}).filtered_videos
   end
 
   def with_same_performance
-    videos = Video.exclude_youtube_id(@video.youtube_id).within_week_of(@video.upload_date)
+    videos = @videos.within_week_of(@video.upload_date)
     leader = @video.leaders.first.slug if @video.leaders.present?
     follower = @video.followers.first.slug if @video.followers.present?
     channel = @video.channel.channel_id
-    Video::Filter.new(videos.includes(Video.search_includes), filtering_params: {channel:, leader:, follower:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {channel:, leader:, follower:, hidden: false}).filtered_videos
   end
 end
