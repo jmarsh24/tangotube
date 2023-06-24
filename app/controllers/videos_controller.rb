5d8bf5a78fc8be525d3cc984f238e39b9e4c64b1
Justin Marsh <jmarsh24@gmail.com>
2023-06-24 22:17:02 +0200

    replace

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -67,14 +67,14 @@ class VideosController < ApplicationController
   # @route PATCH /videos/:id/featured (featured_video)
   def featured
     @video.update!(featured: !@video.featured?)
-    ui.update("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
+    ui.replace("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
   end
 
   # POST /videos/:id/like
   def like
     authorize @video
     current_user.likes.create!(likeable: @video)
-    ui.update("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
+    ui.replace("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
   end
 
   # DELETE /videos/:id/unlike
@@ -82,7 +82,7 @@ class VideosController < ApplicationController
     authorize @video
     like = current_user.likes.find_by(likeable: @video)
     like&.destroy!
-    ui.update("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
+    ui.replace("video_#{@video.id}_vote", with: "videos/show/vote", video: @video)
   end
 
   def filters
