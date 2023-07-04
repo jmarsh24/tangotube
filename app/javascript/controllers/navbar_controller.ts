50949c119fc6b894eadcde6da10325cde7f62e9d
Justin Marsh <jmarsh24@gmail.com>
2023-07-04 09:57:03 +0200

    autohide on mobile for navbar

--- app/javascript/controllers/navbar_controller.ts
+++ app/javascript/controllers/navbar_controller.ts
@@ -1,11 +1,36 @@
 import { Controller } from '@hotwired/stimulus';
 
-export default class extends Controller {
-  menuTarget!: HTMLElement;
+export default class NavbarController extends Controller {
+  lastScrollTop: number = 0;
 
-  static targets = ['menu'];
+  connect() {
+    this.lastScrollTop = 0;
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
 
-  toggle() {
-    this.menuTarget.classList.toggle('open');
+  disconnect() {
+    window.removeEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  handleScroll() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > this.lastScrollTop) {
+      // downscroll code
+      this.hideNavbar();
+    } else {
+      // upscroll code
+      this.showNavbar();
+    }
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  }
+
+  hideNavbar() {
+    // Use the actual height of your navbar
+    // this.element refers to the element this controller is connected to
+    (this.element as HTMLElement).style.top = '-56px';
+  }
+
+  showNavbar() {
+    (this.element as HTMLElement).style.top = '0';
   }
 }
