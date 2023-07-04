92157f658c6a3769a753500a199f79cd9ebe15f0
Justin Marsh <jmarsh24@gmail.com>
2023-07-05 01:14:11 +0200

    Update song matcher

--- app/models/external_video_import/metadata_processing/song_matcher.rb
+++ app/models/external_video_import/metadata_processing/song_matcher.rb
@@ -10,7 +10,22 @@ module ExternalVideoImport
 
       def initialize
         @fuzzy_text = FuzzyText.new
-        @songs = ::Song.joins(:orchestra).active.order(videos_count: :desc).to_a
+        @songs ||= Rails.cache.fetch("songs", expires_in: 24.hours) {
+          ::Song.joins(:orchestra)
+            .active
+            .order(videos_count: :desc)
+            .select(:id, :title, :artist, :last_name_search, :videos_count)
+            .map { |song|
+            {
+              id: song.id,
+              title: normalize(song.title),
+              artist: normalize(song.artist),
+              search_title: normalize(song.search_title),
+              last_name_search: normalize(song.last_name_search),
+              videos_count: song.videos_count
+            }
+          }
+        }
       end
 
       def match(video_title:, video_description:, video_tags: [], song_titles: [], song_albums: [], song_artists: [])
@@ -22,14 +37,15 @@ module ExternalVideoImport
           song_albums:,
           song_artists:
         )
-        artist_search_names = @songs.map(&:last_name_search).uniq.map(&:downcase)
+        artist_search_names = @songs.map { |song| song[:last_name_search] }.uniq
         songs_with_artist_match = search_artist_match(video_data, artist_search_names)
         perfect_match = perfect_search_title_match(video_data, songs_with_artist_match)
         return perfect_match if perfect_match
 
         results = calculate_all_scores(video_data, songs_with_artist_match)
 
-        highest_scoring_match(results)
+        song_hash = highest_scoring_match(results)
+        Song.find(song_hash[:id])
       end
 
       private
@@ -41,7 +57,7 @@ module ExternalVideoImport
 
         song_artists.each do |artist_name|
           if video_artist_metadata.include?(artist_name) || video_title_description.include?(artist_name)
-            songs_with_artist << @songs.select { |song| song.last_name_search.downcase == artist_name }
+            songs_with_artist << @songs.select { |song| song[:last_name_search] == artist_name }
           else
             artist_matches = {
               "artist_song_artist_in_video" => @fuzzy_text.jaro_winkler_score(artist_name.downcase, video_title_description),
@@ -49,7 +65,7 @@ module ExternalVideoImport
             }
 
             if artist_matches["artist_song_artist_in_video"] >= MATCH_THRESHOLD || artist_matches["artist_song_artist_in_metadata"] >= MATCH_THRESHOLD
-              songs_with_artist << @songs.select { |song| song.last_name_search.downcase == artist_name }
+              songs_with_artist << @songs.select { |song| song[:last_name_search] == artist_name }
             end
           end
         end
@@ -60,7 +76,7 @@ module ExternalVideoImport
       def perfect_search_title_match(video_data, filtered_songs)
         video_title_search_string = "#{video_data.title} #{video_data.description} #{video_data.song_titles} #{video_data.song_albums} #{video_data.tags}"
         filtered_songs.each do |song|
-          if video_title_search_string.include?(song.title)
+          if video_title_search_string.include?(song[:title])
             return song
           end
         end
@@ -94,7 +110,7 @@ module ExternalVideoImport
 
       def calculate_scores_for_song(song, video_data)
         start_time = Time.now
-        Rails.logger.info "Calculating scores for song #{song.id}..."
+        Rails.logger.info "Calculating scores for song #{song[:id]}..."
 
         scores = calculate_scores(song, video_data)
 
@@ -112,25 +128,25 @@ module ExternalVideoImport
 
         end_time = Time.now
         elapsed_time = end_time - start_time
-        Rails.logger.info "Finished calculating scores for song #{song.id}. Elapsed time: #{elapsed_time.round(2)} seconds"
+        Rails.logger.info "Finished calculating scores for song #{song[:id]}. Elapsed time: #{elapsed_time.round(2)} seconds"
 
         scores
       end
 
       def highest_scoring_match(results)
         best_matches = results.select { |song, scores| scores["is_match"] }
-        highest_scoring_song = best_matches.max_by { |song, scores| [scores.values_at("title_score", "artist_score").sum, song.videos_count] }
+        highest_scoring_song = best_matches.max_by { |song, scores| [scores.values_at("title_score", "artist_score").sum, song[:videos_count]] }
 
         highest_scoring_song&.first
       end
 
       def calculate_scores(song, video_data)
-        song_title = normalize(song.title)
-        song_search_title = normalize(song.search_title)
-        song_artist = normalize(song.artist)
+        song_title = song[:title]
+        song_search_title = song[:search_title]
+        song_artist = song[:artist]
         video_title_description = [normalize(video_data.title), normalize(video_data.description)].join(" ")
 
-        perfect_artist_match = video_title_description.include?(song.last_name_search.downcase)
+        perfect_artist_match = video_title_description.include?(song[:last_name_search])
 
         {
           "title_song_title_in_video" => @fuzzy_text.jaro_winkler_score(song_title, video_title_description),
