b565d2669c08bcc2c4349853aaaf8b08c2edfe18
Justin Marsh <jmarsh24@gmail.com>
2023-07-09 11:27:57 +0200

    Fix search broken on switcher type and also style x

--- app/models/search.rb
+++ app/models/search.rb
@@ -18,32 +18,33 @@ class Search
   end
 
   def results
-    @results ||= search_results
+    @results ||=
+      if category == "all"
+        all_results
+      elsif MODELS.key?(category)
+        results_for_category(category)
+      else
+        []
+      end
   end
 
   private
 
-  def search_results
-    if category == "all"
-      all_results
-    elsif MODELS.key?(category)
-      results_for_category(category)
-    else
-      []
-    end
-  end
-
   def results_for_category(category)
     results = MODELS[category].search(term)
-    results.any? ? results.limit(DEFAULT_LIMIT) : []
+    results.any? ? group_by_model(results.limit(DEFAULT_LIMIT)) : []
   end
 
   def all_results
-    results_for_all_models(DEFAULT_LIMIT).group_by { |result| result.class.name }
+    group_by_model(results_for_all_models(DEFAULT_LIMIT))
   end
 
   def results_for_all_models(limit)
     models_with_results = MODELS.values.select { |model| model.search(term).any? }
     models_with_results.flat_map { |model| model.search(term).limit(limit) }
+  end
+
+  def group_by_model(results)
+    results.group_by { |result| result.class.name }
   end
 end
