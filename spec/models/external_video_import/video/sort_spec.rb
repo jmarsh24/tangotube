05343ead0228b81ef2b9eb4ea12617b62ea7c6b0
Justin Marsh <jmarsh24@gmail.com>
2023-07-10 19:58:39 +0200

    Fix sort spec

--- spec/models/external_video_import/video/sort_spec.rb
+++ spec/models/external_video_import/video/sort_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "rails_helper"
 
 RSpec.describe Video::Sort do
@@ -10,47 +8,39 @@ RSpec.describe Video::Sort do
       it "sorts by popularity in descending order" do
         sorted_videos = Video::Sort.new(Video.all).sorted_videos
 
-        expect(sorted_videos).to match_array(Video.all.order(popularity: :desc))
+        expect(sorted_videos).to eq(Video.all.order(popularity: :desc))
       end
     end
 
     context "when sorting by upload_date in ascending order" do
       it "sorts the videos by upload date in ascending order" do
-        sorted_videos = Video::Sort.new(Video.all, sorting_params: {sort: "upload_date", direction: "asc"}).sorted_videos
+        sorted_videos = Video::Sort.new(Video.all, sort: "oldest").sorted_videos
 
-        expect(sorted_videos).to match_array(Video.all.order(upload_date: :asc))
+        expect(sorted_videos).to eq(Video.all.order(upload_date: :asc))
       end
     end
 
     context "when sorting by song title in ascending order" do
       it "sorts the videos by song title in ascending order" do
-        sorted_videos = Video::Sort.new(Video.all, sorting_params: {sort: "song", direction: "asc"}).sorted_videos
+        sorted_videos = Video::Sort.new(Video.all, sort: "song").sorted_videos
 
-        expect(sorted_videos).to match_array(Video.all.joins(:song).order("songs.title": :asc))
+        expect(sorted_videos).to eq(Video.all.joins(:song).order("songs.title": :asc))
       end
     end
 
     context "when sorting by orchestra in descending order" do
       it "sorts the videos by orchestra in descending order" do
-        sorted_videos = Video::Sort.new(Video.all, sorting_params: {sort: "orchestra", direction: "desc"}).sorted_videos
+        sorted_videos = Video::Sort.new(Video.all, sort: "orchestra").sorted_videos
 
-        expect(sorted_videos).to match_array(Video.all.joins(song: :orchestra).order("orchestras.name DESC"))
+        expect(sorted_videos).to eq(Video.all.joins(song: :orchestra).order("orchestras.name": :asc))
       end
     end
 
     context "when sorting by performance in ascending order" do
       it "sorts the videos by performance" do
-        sorted_videos = Video::Sort.new(Video.all, sorting_params: {sort: "performance", direction: "desc"}).sorted_videos
+        sorted_videos = Video::Sort.new(Video.all, sort: "performance").sorted_videos
 
-        expect(sorted_videos).to match_array(Video.all.joins(:performance_video).order("performance_videos.performance_id ASC"))
-      end
-    end
-
-    context "when sorting by orchestra in desc order" do
-      it "sorts the videos by orchestra in desc order" do
-        sorted_videos = Video::Sort.new(Video.all, sorting_params: {sort: "orchestra", direction: "desc"}).sorted_videos
-
-        expect(sorted_videos).to match_array(Video.all.joins(song: :orchestra).order("orchestras.name DESC"))
+        expect(sorted_videos).to eq(Video.all.joins(:performance_video).order("performance_videos.performance_id": :desc))
       end
     end
   end
