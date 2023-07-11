be712ea5c81f4864c0231ed781c7254cc2d0e7de
Justin Marsh <jmarsh24@gmail.com>
2023-07-11 17:16:25 +0200

    Fix related videos spec

--- app/models/video/related_videos.rb
+++ app/models/video/related_videos.rb
@@ -11,25 +11,25 @@ class Video::RelatedVideos
   def with_same_dancers
     leader = @video.leaders.first.slug
     follower = @video.followers.first.slug
-    Video::Filter.new(@videos, filtering_params: {leader:, follower:, hidden: false}).filtered_videos
+    Video::Filter.new(@videos, filtering_params: {leader:, follower:, hidden: false}, excluded_youtube_id: @video.youtube_id).filtered_videos
   end
 
   def with_same_event
     videos = @videos.within_week_of(@video.upload_date)
     event = @video.event.slug
-    Video::Filter.new(videos, filtering_params: {event:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {event:, hidden: false}, excluded_youtube_id: @video.youtube_id).filtered_videos
   end
 
   def with_same_song
     videos = @videos.has_leader.has_follower
     song = @video.song.slug
-    Video::Filter.new(videos, filtering_params: {song:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {song:, hidden: false}, excluded_youtube_id: @video.youtube_id).filtered_videos
   end
 
   def with_same_channel
     videos = @videos.has_leader.has_follower
     channel = @video.channel.channel_id
-    Video::Filter.new(videos, filtering_params: {channel:, hidden: false}).filtered_videos
+    Video::Filter.new(videos, filtering_params: {channel:, hidden: false}, excluded_youtube_id: @video.youtube_id).filtered_videos
   end
 
   def with_same_performance
