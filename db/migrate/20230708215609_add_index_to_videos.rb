e66439ffcc5c0d6b6c19b17d341a3f43d2884dfd
Justin Marsh <jmarsh24@gmail.com>
2023-07-12 10:22:30 +0200

    remove duplicate index

--- db/migrate/20230708215609_add_index_to_videos.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexToVideos < ActiveRecord::Migration[7.0]
-  def change
-    add_index :videos, :index, using: :gist, opclass: :gist_trgm_ops
-  end
-end
