d6269aae741c039f29709660cf2b0912c390cf3c
Justin Marsh <jmarsh24@gmail.com>
2023-07-12 10:06:43 +0200

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@
 
 Sidekiq.configure_server do |config|
   config.redis = {url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0")}
+  config.concurrency = ENV.fetch("SIDEKIQ_CONCURRENCY", 5).to_i
 end
 
 Sidekiq.configure_client do |config|
