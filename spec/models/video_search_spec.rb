087fa714a38b0ebae13e45ffdf697b0706002d17
Justin Marsh <jmarsh24@gmail.com>
2023-07-15 14:37:56 +0200

    add video search test

---
+++ spec/models/video_search_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe VideoSearch do
+  fixtures :all
+
+  describe "#search" do
+    it "finds results" do
+      videos = [videos(:video_1_featured), videos(:video_4_featured)]
+
+      expect(VideoSearch.search("carlitos espinoza")).to eq []
+
+      VideoSearch.refresh
+
+      results = VideoSearch.search("carlitos espinoza")
+      expect(results).to match_array(videos.map(&:id))
+    end
+  end
+end
