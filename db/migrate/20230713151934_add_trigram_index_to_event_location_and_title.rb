6464c614927bf6ee1fca2f32f84eb703a8408d9b
Justin Marsh <jmarsh24@gmail.com>
2023-07-16 13:11:54 +0200

    working search

--- db/migrate/20230713151934_add_trigram_index_to_event_location_and_title.rb
+++ db/migrate/20230713151934_add_trigram_index_to_event_location_and_title.rb
@@ -3,8 +3,8 @@
 class AddTrigramIndexToEventLocationAndTitle < ActiveRecord::Migration[7.0]
   def change
     remove_index :events, :title
-    add_index :events, :title, using: :gin, opclass: :gin_trgm_ops, name: "index_events_on_title_trigram"
-    add_index :events, :city, using: :gin, opclass: :gin_trgm_ops, name: "index_events_on_city_trigram"
-    add_index :events, :country, using: :gin, opclass: :gin_trgm_ops, name: "index_events_on_country_trigram"
+    add_index :events, :title, using: :gist, opclass: :gist_trgm_ops, name: "index_events_on_title_trigram"
+    add_index :events, :city, using: :gist, opclass: :gist_trgm_ops, name: "index_events_on_city_trigram"
+    add_index :events, :country, using: :gist, opclass: :gist_trgm_ops, name: "index_events_on_country_trigram"
   end
 end
