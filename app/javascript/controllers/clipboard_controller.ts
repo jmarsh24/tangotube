53ce0690cb55a8137abb401808d49ee6b8f7402c
Justin Marsh <jmarsh24@gmail.com>
2023-07-16 11:50:49 +0200

    remove unused stimulus controllers

--- app/javascript/controllers/clipboard_controller.ts
+++ app/javascript/controllers/clipboard_controller.ts
@@ -8,18 +8,4 @@ export default class extends Controller {
     this.sourceTarget.select();
     navigator.clipboard.writeText(this.sourceTarget.value);
   }
-
-  // showCopyNotification() {
-  //   // Replace this with your preferred notification library or implementation
-  //   // Here's an example using a basic toast notification with Bulma CSS classes
-  //   const notification = document.createElement('div');
-  //   notification.classList.add('notification', 'is-success');
-  //   notification.textContent = 'Link copied to clipboard!';
-  //   document.body.appendChild(notification);
-
-  //   // Remove the notification after a delay (e.g., 3 seconds)
-  //   setTimeout(() => {
-  //     notification.remove();
-  //   }, 3000);
-  // }
 }
