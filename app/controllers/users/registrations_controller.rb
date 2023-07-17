bed670f3ceb86c02a3e6a4d7884d23beb87e0133
Justin Marsh <jmarsh24@gmail.com>
2023-07-17 03:09:55 +0200

    modalize the devise flow

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout false, only: [:new, :create]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def update_resource(resource, params)
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  layout false, only: [:new, :create]
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
