a6dfd1e105ea960e48bcc996648731ba32c97a44
Justin Marsh <jmarsh24@gmail.com>
2023-07-17 04:02:19 +0200

    layout false for devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout false, only: [:new, :create]
+  layout false
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def update_resource(resource, params)
