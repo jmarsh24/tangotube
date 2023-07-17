bba0161b4c7a98d04d1eabd0e879e9736910cb82
Justin Marsh <jmarsh24@gmail.com>
2023-07-17 04:28:01 +0200

    remove devise

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -3,8 +3,6 @@
 require_relative "controller_macros" # or require_relative './controller_macros' if write in `spec/support/devise.rb`
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
-  config.responder.error_status = :unprocessable_entity
-  config.responder.redirect_status = :see_other
 end
