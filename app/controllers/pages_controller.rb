6dd5c99a1b79cb8637588bfac91db7e9767519cc
Justin Marsh <jmarsh24@gmail.com>
2023-07-17 04:29:10 +0200

    re add pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  # @route GET /privacy (privacy)
+  def privacy
+  end
+
+  # @route GET /terms (terms)
+  def terms
+  end
+end
