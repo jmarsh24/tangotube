28e4d7fdc6cd802f548c64256cafe9438cf02a36
Justin Marsh <jmarsh24@gmail.com>
2023-07-18 21:41:39 +0200

    Don't show related videos if they don't exist

--- app/controllers/related_videos_controller.rb
+++ app/controllers/related_videos_controller.rb
@@ -5,6 +5,7 @@ class RelatedVideosController < ApplicationController
     @type = params[:type]
     @header = t(".header_#{params[:type]}")
     @related_videos = case params[:type]
+
     when "same_dancers"
       related_videos_service.with_same_dancers
     when "same_event"
@@ -15,6 +16,7 @@ class RelatedVideosController < ApplicationController
       related_videos_service.with_same_channel
     when "same_performance"
       related_videos_service.with_same_performance
-    end.includes(:channel, :song, dancer_videos: :dancer, thumbnail_attachment: :blob)
+    end
+    @related_videos = related_videos ? related_videos.includes(:channel, :song, dancer_videos: :dancer, thumbnail_attachment: :blob) : []
   end
 end
