e10a714d51cfade32fbc2afa93eda1c3c295955b
Justin Marsh <jmarsh24@gmail.com>
2023-08-12 22:05:01 +0200

    improve results and link to leader or follower instead of dancer

--- app/models/video_search.rb
+++ app/models/video_search.rb
@@ -27,22 +27,20 @@ class VideoSearch < ApplicationRecord
       sanitized_term = ActiveRecord::Base.connection.quote_string(term)
       select(
         "video_searches.video_id",
-        "(
-      1.0 * (dancer_names <-> '#{sanitized_term}') +
-      1.0 * (channel_title <-> '#{sanitized_term}') +
-      1.0 * (song_title <-> '#{sanitized_term}') +
-      1.0 * (song_artist <-> '#{sanitized_term}') +
-      1.0 * (orchestra_name <-> '#{sanitized_term}') +
-      1.0 * (event_city <-> '#{sanitized_term}') +
-      1.0 * (event_title <-> '#{sanitized_term}') +
-      1.0 * (event_country <-> '#{sanitized_term}') +
-      1.0 * (video_title <-> '#{sanitized_term}') +
-      1.0 * CAST((video_description_vector @@ plainto_tsquery('#{sanitized_term}')) AS INTEGER)) +
-      2.0 * video_scores.score_1 AS total_score"
+        "(0.2 * (dancer_names <-> '#{sanitized_term}') +
+      0.2 * (channel_title <-> '#{sanitized_term}') +
+      0.2 * (song_title <-> '#{sanitized_term}') +
+      0.2 * (song_artist <-> '#{sanitized_term}') +
+      0.2 * (orchestra_name <-> '#{sanitized_term}') +
+      0.2 * (event_city <-> '#{sanitized_term}') +
+      0.2 * (event_title <-> '#{sanitized_term}') +
+      0.2 * (event_country <-> '#{sanitized_term}') +
+      0.2 * (video_title <-> '#{sanitized_term}') +
+      0.2 * CAST((video_description_vector @@ plainto_tsquery('#{sanitized_term}')) AS INTEGER)) +
+      3.0 * video_scores.score_1 AS total_score"
       )
         .joins("INNER JOIN video_scores ON video_scores.video_id = video_searches.video_id")
-        .where(
-          "'#{sanitized_term}' % dancer_names OR
+        .where("'#{sanitized_term}' % dancer_names OR
      '#{sanitized_term}' % channel_title OR
      '#{sanitized_term}' % song_title OR
      '#{sanitized_term}' % song_artist OR
@@ -51,13 +49,12 @@ class VideoSearch < ApplicationRecord
      '#{sanitized_term}' % event_title OR
      '#{sanitized_term}' % event_country OR
      '#{sanitized_term}' <% video_title OR
-     video_description_vector @@ plainto_tsquery('#{sanitized_term}')"
-        )
+     video_description_vector @@ plainto_tsquery('#{sanitized_term}')")
     end
 
     def search(term)
       Video.joins("INNER JOIN (#{subquery(term).to_sql}) AS subquery ON videos.id = subquery.video_id")
-        .order("subquery.total_score ASC")
+        .order("subquery.total_score DESC")
     end
 
     def refresh
