8e4a821008289d4a9d235cb33bccfe067519e84c
Justin Marsh <jmarsh24@gmail.com>
2023-08-13 11:56:34 +0200

    temp prevent statement timeout

--- db/migrate/20230808213706_update_video_searches_to_version_5.rb
+++ db/migrate/20230808213706_update_video_searches_to_version_5.rb
@@ -2,6 +2,8 @@
 
 class UpdateVideoSearchesToVersion5 < ActiveRecord::Migration[7.0]
   def change
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 0")
     update_view :video_searches, version: 5, revert_to_version: 4, materialized: true
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 60000")
   end
 end
--- db/migrate/20230812114356_update_video_searches_to_version_6.rb
+++ db/migrate/20230812114356_update_video_searches_to_version_6.rb
@@ -2,7 +2,9 @@
 
 class UpdateVideoSearchesToVersion6 < ActiveRecord::Migration[7.0]
   def change
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 0")
     update_view :video_searches, version: 6, revert_to_version: 5, materialized: true
     add_index :video_searches, :video_description_vector, using: :gin
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 60000")
   end
 end
