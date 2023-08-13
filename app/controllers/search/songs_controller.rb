8227e93ea289fefd5bddd327d4f2591c277f0537
Justin Marsh <jmarsh24@gmail.com>
2023-08-13 22:41:23 +0200

    add preloads to songs controller

--- app/controllers/search/songs_controller.rb
+++ app/controllers/search/songs_controller.rb
@@ -5,7 +5,7 @@ class Search::SongsController < ApplicationController
   def index
     @songs =
       if params[:query].present?
-        Song.search(params[:query]).limit(10)
+        Song.search(params[:query]).preloads(:orchestra).limit(10)
       else
         Song.all.limit(10).most_popular
       end
