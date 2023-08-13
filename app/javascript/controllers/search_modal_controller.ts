1d824d40ccadeb63348d3b8c85d2c8b5116a98f6
Justin Marsh <jmarsh24@gmail.com>
2023-08-13 11:33:08 +0200

    working window!

--- app/javascript/controllers/search_modal_controller.ts
+++ app/javascript/controllers/search_modal_controller.ts
@@ -3,22 +3,15 @@ import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="search-modal"
 export default class extends Controller {
   static targets = ['modal'];
-  // static values = { searchPath: String };
-
   modalTarget!: HTMLElement;
-  // searchPathValue!: string;
-
-  connect(): void {
-    // console.log('Search modal controller connected...');
-    // console.log(this.searchPathValue);
-  }
 
   show(): void {
-    // this.modalTarget.setAttribute('src', this.searchPathValue);
-    this.modalTarget.style.display = 'block';
+    document.body.classList.add('search-window--open');
+    this.modalTarget.classList.add('search-window--open');
   }
 
   close(): void {
-    this.modalTarget.style.display = 'none';
+    document.body.classList.remove('search-window--open');
+    this.modalTarget.classList.remove('search-window--open');
   }
 }
