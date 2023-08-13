e489de0555c8d4b9b68f6a864f08212b07d7c5e1
Justin Marsh <jmarsh24@gmail.com>
2023-08-13 15:17:03 +0200

    Fix import all channels job

--- app/jobs/import_new_videos_for_all_channels_job.rb
+++ app/jobs/import_new_videos_for_all_channels_job.rb
@@ -3,7 +3,9 @@
 class ImportNewVideosForAllChannelsJob < ApplicationJob
   queue_as :default
 
-  def perform(use_scraper: false, use_music_recognizer: false)
+  def perform(options = {})
+    use_scraper = options.fetch(:use_scraper, false)
+    use_music_recognizer = options.fetch(:use_music_recognizer, false)
     Channel.active.find_each do |channel|
       channel.import_new_videos(use_scraper:, use_music_recognizer:)
     end
