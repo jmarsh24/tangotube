38cf2278d04455da40a4a391adba4ac6d4f64705
Justin Marsh <jmarsh24@gmail.com>
2023-08-25 20:26:57 +0200

    song matcher works

--- app/models/external_video_import/metadata_processing/song_matcher.rb
+++ app/models/external_video_import/metadata_processing/song_matcher.rb
@@ -6,34 +6,34 @@ module ExternalVideoImport
       MATCH_THRESHOLD = 0.9
 
       def match(video_title:, video_description:, video_tags: [], song_titles: [], song_albums: [], song_artists: [])
-        artist_ids = find_artists([song_artists, video_title, video_tags, video_description, song_albums])
-        binding.irb
-        if artist_ids.empty? && song_titles.first.present? && song_artists.first.present?
+        song_ids = find_artists([song_artists, video_title, video_tags, video_description, song_albums])
+
+        if song_ids.empty? && song_titles.first.present? && song_artists.first.present?
           return Song.create!(title: song_titles.first, artist: song_artists.first, genre: "Alternative")
         end
 
-        find_song(artist_ids, [video_title, video_description, video_tags, song_titles])
+        find_song(song_ids, [video_title, video_description, video_tags, song_titles])
       end
 
       private
 
       def find_artists(text)
         text = text.map { |text| TextNormalizer.normalize(text.to_s) }
-        artist_id_names = Song.group(:artist).pluck(Arel.sql("MAX(id)"), :artist)
-        artist_ids = []
+        song_id_names = Song.group(:artist).pluck(Arel.sql("MAX(id)"), :artist)
+        song_ids = []
 
-        artist_id_names.each do |id, artist_name|
+        song_id_names.each do |id, artist_name|
           text.reject(&:empty?).each do |text|
             ratio = FuzzyText.new.jaro_winkler_score(needle: artist_name, haystack: text)
-            artist_ids << id if ratio > MATCH_THRESHOLD
+            song_ids << id if ratio > MATCH_THRESHOLD
           end
         end
-        artist_ids.uniq
+        song_ids.uniq
       end
 
-      def find_song(orchestra_ids, text)
+      def find_song(song_ids, text)
         text = text.map { |text| TextNormalizer.normalize(text.to_s) }
-        song_ids_title = Song.active.where(orchestra_id: orchestra_ids).pluck(:id, :title)
+        song_ids_title = Song.active.where(id: song_ids).pluck(:id, :title)
         song_ids_title = song_ids_title.map! { |id, title| [id, TextNormalizer.normalize(title)] }
 
         scores_and_songs = song_ids_title.flat_map do |id, title|
