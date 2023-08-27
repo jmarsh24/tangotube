433131bbe487f01333bfcc78952a91737708327c
Justin Marsh <jmarsh24@gmail.com>
2023-08-27 19:30:21 +0200

    update queue

--- app/jobs/update_video_job.rb
+++ app/jobs/update_video_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UpdateVideoJob < ApplicationJob
-  queue_as :low_priority
+  queue_as :very_low_priority
 
   def perform(video, use_scraper: false, use_music_recognizer: false)
     ExternalVideoImport::Importer.new.update(video, use_scraper:, use_music_recognizer:)
