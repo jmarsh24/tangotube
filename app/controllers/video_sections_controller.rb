60f65a64077a02de1120ff0a0dd1d6fd380c0546
Justin Marsh <jmarsh24@gmail.com>
2023-08-29 09:50:55 +0200

    don't show hidden

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -2,19 +2,23 @@ class VideoSectionsController < ApplicationController
   helper_method :filtering_params
 
   def recent
-    @videos = Video::Search.new(filtering_params:, sort: "most_recent", user: current_user).videos.limit(36).preload(Video.search_includes)
+    @videos = Video::Search.new(filtering_params:, sort: "most_recent", user: current_user).videos
+      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
   end
 
   def oldest
-    @videos = Video::Search.new(filtering_params:, sort: "trending_6", user: current_user).videos.limit(36).preload(Video.search_includes)
+    @videos = Video::Search.new(filtering_params:, sort: "trending_6", user: current_user).videos
+      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
   end
 
   def performances
-    @videos = Video::Search.new(filtering_params:, sort: "performance", user: current_user).videos.limit(36).preload(Video.search_includes)
+    @videos = Video::Search.new(filtering_params:, sort: "performance", user: current_user).videos
+      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
   end
 
   def trending
-    @videos = Video::Search.new(filtering_params:, sort: "trending_5", user: current_user).videos.limit(36).preload(Video.search_includes)
+    @videos = Video::Search.new(filtering_params:, sort: "trending_5", user: current_user).videos
+      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
   end
 
   private
