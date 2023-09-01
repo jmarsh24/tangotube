ed529fa0f14bdedf0103fd74103f687b30bdc8fc
Justin Marsh <jmarsh24@gmail.com>
2023-09-01 11:11:54 +0200

    use left outer joins

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -3,34 +3,50 @@ class VideoSectionsController < ApplicationController
 
   def recent
     @videos = Video::Search.new(filtering_params:, sort: "most_recent", user: current_user).videos
-      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   def oldest
     @videos = Video::Search.new(filtering_params:, sort: "trending_6", user: current_user).videos
-      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   def performances
     @videos = Video::Search.new(filtering_params:, sort: "performance", user: current_user).videos
-      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   def trending
     @videos = Video::Search.new(filtering_params:, sort: "trending_5", user: current_user).videos
-      .not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   def random_event
     @event = Event.most_popular.limit(24).sample
     @year = @event.videos.pluck(:upload_date_year).uniq.sample
     @videos = Video::Search.new(filtering_params: {event: @event.slug, year: @year}, sort: "trending_5", user: current_user).videos
-      .has_leader.has_follower.not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .left_outer_joins(:dancer_videos)
+      .where.not(dancer_videos: {id: nil})
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   def alternative
     @videos = Video::Search.new(filtering_params: {genre: "alternative"}, user: current_user).videos
-      .has_leader.has_follower.not_hidden.from_active_channels.limit(36).preload(Video.search_includes)
+      .left_outer_joins(:dancer_videos)
+      .where.not(dancer_videos: {id: nil})
+      .not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
   end
 
   private
