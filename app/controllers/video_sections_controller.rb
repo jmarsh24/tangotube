f4b17efd7d3e9407c5179cc050a224f1ae826387
Justin Marsh <jmarsh24@gmail.com>
2023-09-01 16:23:05 +0200

    only show videos with dancer

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -3,28 +3,28 @@ class VideoSectionsController < ApplicationController
 
   def recent
     @videos = Video::Search.new(filtering_params:, sort: "most_recent", user: current_user).videos
-      .not_hidden.from_active_channels
+      .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def oldest
     @videos = Video::Search.new(filtering_params:, sort: "trending_6", user: current_user).videos
-      .not_hidden.from_active_channels
+      .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def performances
     @videos = Video::Search.new(filtering_params:, sort: "performance", user: current_user).videos
-      .not_hidden.from_active_channels
+      .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def trending
     @videos = Video::Search.new(filtering_params:, sort: "trending_5", user: current_user).videos
-      .not_hidden.from_active_channels
+      .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
