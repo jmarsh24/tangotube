a3ff63f8acd0a044ba1fbf343b07ce9792bd9d1e
Justin Marsh <jmarsh24@gmail.com>
2023-09-04 01:29:52 +0200

    add nested section

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -2,28 +2,28 @@ class VideoSectionsController < ApplicationController
   helper_method :filtering_params
 
   def recent
-    @videos = Video::Search.new(filtering_params:, sort: "most_recent", user: current_user).videos
+    @videos = Video::Search.new(filtering_params:, sort: "most_recent").videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def oldest
-    @videos = Video::Search.new(filtering_params:, sort: "trending_6", user: current_user).videos
+    @videos = Video::Search.new(filtering_params:, sort: "trending_6").videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def performances
-    @videos = Video::Search.new(filtering_params:, sort: "performance", user: current_user).videos
+    @videos = Video::Search.new(filtering_params:, sort: "performance").videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def trending
-    @videos = Video::Search.new(filtering_params:, sort: "trending_5", user: current_user).videos
+    @videos = Video::Search.new(filtering_params:, sort: "trending_5").videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -32,14 +32,14 @@ class VideoSectionsController < ApplicationController
   def random_event
     @event = Event.most_popular.limit(8).sample
     @year = @event.videos.pluck(:upload_date_year).uniq.sample
-    @videos = Video::Search.new(filtering_params: {event: @event.slug, year: @year}, sort: "trending_5", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {event: @event.slug, year: @year}, sort: "trending_5").videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
   end
 
   def alternative
-    @videos = Video::Search.new(filtering_params: {genre: "alternative"}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {genre: "alternative"}).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
