ee57bc3094601f8e09f4a8a3d3974e03ecfde962
Justin Marsh <jmarsh24@gmail.com>
2023-09-08 19:16:20 +0200

    update event matcher

--- app/models/external_video_import/metadata_processing/event_matcher.rb
+++ app/models/external_video_import/metadata_processing/event_matcher.rb
@@ -10,18 +10,21 @@ module ExternalVideoImport
         event_id_titles = Event.all.pluck(:id, :title)
         event_id_titles.map! { |id, title| [id, TextNormalizer.normalize(title)] }
 
-        matched_event = nil
+        max_ratio = 0
+        best_match_event_id = nil
+
         event_id_titles.each do |id, title|
           trigram_instance = Trigram.new(normalized_text)
           ratio = trigram_instance.similarity(title)
-          if ratio > MATCH_THRESHOLD
-            matched_event = Event.find(id)
-            break
+          if ratio > max_ratio
+            max_ratio = ratio
+            best_match_event_id = id
           end
         end
 
-        if matched_event
-          Rails.logger.debug "Matched event: #{matched_event.title}"
+        if max_ratio > MATCH_THRESHOLD
+          matched_event = Event.find(best_match_event_id)
+          Rails.logger.debug "Matched event: #{matched_event.title} with ratio: #{max_ratio}"
           matched_event
         else
           Rails.logger.debug "No event matched."
