9469e02e508aa57d5e5b446e65f8e10ea5747e86
Justin Marsh <jmarsh24@gmail.com>
2023-09-25 17:55:57 +0200

    don't call watchers without current_user

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -3,7 +3,9 @@ class WatchersController < ApplicationController
   def has_been_watched
     if turbo_frame_request?
       video_id = params[:id]
-      render "watched_status", locals: {watched: current_user.watches.exists?(video_id:), video_id:}
+      if current_user
+        render "watched_status", locals: {watched: current_user.watches.exists?(video_id:), video_id:}
+      end
     end
   end
 end
