5e8eb6347acea2033014eb1c87a31bd2c6882069
Justin Marsh <jmarsh24@gmail.com>
2023-09-28 15:50:42 +0200

    add take

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -53,6 +53,42 @@ class VideoSectionsController < ApplicationController
       .preload(Video.search_includes)
   end
 
+  def dancer
+    @dancers = Dancer.most_popular.with_attached_profile_image.limit(128).shuffle.take(24)
+    @dancer = @dancers.sample
+    @videos = Video::Search.new(filtering_params: {dancer: @dancer.slug}, user: current_user).videos
+      .has_leader.has_follower.not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
+  end
+
+  def song
+    @songs = Song.most_popular.joins(:leaders, :followers).preload(:orchestra).limit(24).shuffle.take(1)
+    @song = @songs.sample
+    @videos = Video::Search.new(filtering_params: {song: @song.slug}, user: current_user).videos
+      .has_leader.has_follower.not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
+  end
+
+  def channel
+    @channels = Channel.most_popular.active.with_attached_thumbnail.limit(12).shuffle.take(1)
+    @channel = @channels.sample
+    @videos = Video::Search.new(filtering_params: {channel: @channel.youtube_slug}, user: current_user).videos
+      .has_leader.has_follower.not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
+  end
+
+  def orchestra
+    @orchestras = Orchestra.most_popular.with_attached_profile_image.limit(24).shuffle.take(1)
+    @orchestra = @orchestras.sample
+    @videos = Video::Search.new(filtering_params: {orchestra: @orchestra.slug}, user: current_user).videos
+      .has_leader.has_follower.not_hidden.from_active_channels
+      .limit(36)
+      .preload(Video.search_includes)
+  end
+
   private
 
   def filtering_params
