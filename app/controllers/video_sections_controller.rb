df1444aa20e60e5f1f6e05c97456d228b0ae8d3d
Justin Marsh <jmarsh24@gmail.com>
2023-09-30 14:48:23 +0200

    add ability to like

--- app/controllers/video_sections_controller.rb
+++ app/controllers/video_sections_controller.rb
@@ -5,7 +5,7 @@ class VideoSectionsController < ApplicationController
 
   # @route GET /video_sections/recent (recent_video_sections)
   def recent
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched]}, sort: "most_recent", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked]}, sort: "most_recent", user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -13,7 +13,7 @@ class VideoSectionsController < ApplicationController
 
   # @route GET /video_sections/older (older_video_sections)
   def older
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched]}, sort: "oldest", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked]}, sort: "oldest", user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -21,7 +21,7 @@ class VideoSectionsController < ApplicationController
 
   # @route GET /video_sections/performances (performances_video_sections)
   def performances
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched]}, sort: "performance", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked]}, sort: "performance", user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -31,7 +31,7 @@ class VideoSectionsController < ApplicationController
   def event
     @events = Event.most_popular.limit(8)
     @event = @events.sample
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], event: @event.slug, year: @year}, sort: "trending_5", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], event: @event.slug, year: @year}, sort: "trending_5", user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -39,7 +39,7 @@ class VideoSectionsController < ApplicationController
 
   # @route GET /video_sections/trending (trending_video_sections)
   def trending
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched]}, sort: "trending_5", user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked]}, sort: "trending_5", user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -47,7 +47,7 @@ class VideoSectionsController < ApplicationController
 
   # @route GET /video_sections/alternative (alternative_video_sections)
   def alternative
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], genre: "alternative"}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], genre: "alternative"}, user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -57,7 +57,7 @@ class VideoSectionsController < ApplicationController
   def dancer
     @dancers = Dancer.most_popular.with_attached_profile_image.limit(128).shuffle.take(24)
     @dancer = @dancers.sample
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], dancer: @dancer.slug}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], dancer: @dancer.slug}, user: current_user).videos
       .has_leader.has_follower.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -71,7 +71,7 @@ class VideoSectionsController < ApplicationController
         .arel.exists
     ).order(videos_count: :desc).limit(48).take(24).shuffle
     @song = @songs.sample
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], song: @song.slug}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], song: @song.slug}, user: current_user).videos
       .has_leader.has_follower.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -81,7 +81,7 @@ class VideoSectionsController < ApplicationController
   def channel
     @channels = Channel.most_popular.active.with_attached_thumbnail.limit(12).shuffle
     @channel = @channels.sample
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], channel: @channel.youtube_slug}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], channel: @channel.youtube_slug}, user: current_user).videos
       .has_leader.has_follower.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -91,7 +91,7 @@ class VideoSectionsController < ApplicationController
   def orchestra
     @orchestras = Orchestra.most_popular.with_attached_profile_image.limit(24).shuffle
     @orchestra = @orchestras.sample
-    @videos = Video::Search.new(filtering_params: {watched: params[:watched], orchestra: @orchestra.slug}, user: current_user).videos
+    @videos = Video::Search.new(filtering_params: {watched: params[:watched], liked: params[:liked], orchestra: @orchestra.slug}, user: current_user).videos
       .has_leader.has_follower.not_hidden.from_active_channels
       .limit(36)
       .preload(Video.search_includes)
@@ -99,7 +99,7 @@ class VideoSectionsController < ApplicationController
 
   def interview
     interview_videos = Video.title_match(["entrevista", "interview", "tengo una pregunta para vos", "podcast", "tango musicality", "documentary", "tango history", "opinions", "tango music visualization", "tango magazine", "what is tango freestyle"])
-    @videos = Video::Filter.new(interview_videos, filtering_params: {watched: params[:watched]}, user: current_user).videos
+    @videos = Video::Filter.new(interview_videos, filtering_params: {watched: params[:watched], liked: params[:liked]}, user: current_user).videos
       .has_dancer.not_hidden.from_active_channels
       .limit(500)
       .preload(Video.search_includes)
@@ -108,7 +108,7 @@ class VideoSectionsController < ApplicationController
 
   def workshop
     class_videos = Video.title_match(["workshop", "class", "clase", "resume", "musicality", "demo", "sacadas", "giros", "colgadas", "technique", "variacion"])
-    @videos = Video::Filter.new(class_videos, filtering_params: {watched: params[:watched]}, user: current_user).videos
+    @videos = Video::Filter.new(class_videos, filtering_params: {watched: params[:watched], liked: params[:liked]}, user: current_user).videos
       .not_hidden.from_active_channels
       .limit(500)
       .preload(Video.search_includes)
@@ -117,7 +117,7 @@ class VideoSectionsController < ApplicationController
 
   def mundial
     mundial_videos = Video.where("videos.title ILIKE ?", "%mundial de tango 2023%")
-    @videos = Video::Filter.new(mundial_videos, filtering_params: {watched: params[:watched]}, user: current_user).videos
+    @videos = Video::Filter.new(mundial_videos, filtering_params: {watched: params[:watched], liked: params[:liked]}, user: current_user).videos
       .not_hidden.from_active_channels
       .limit(500)
       .preload(Video.search_includes)
