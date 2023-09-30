ae03b74a1f43310a28d04faa10d4e9e3f6e22596
Justin Marsh <jmarsh24@gmail.com>
2023-09-30 17:08:02 +0200

    update job

--- app/jobs/create_image_preview_job.rb
+++ app/jobs/create_image_preview_job.rb
@@ -1,42 +1,41 @@
 # frozen_string_literal: true
 
-class CreateImagePreviewJob < ApplicationJob
-  queue_as :active_storage_analysis
-  sidekiq_options retry: false
+ class CreateImagePreviewJob < ActiveJob::Base
+    queue_as :active_storage_analysis
 
-  def perform(attachment_id)
-    attachment = ActiveStorage::Attachment.find(attachment_id)
+    def perform(attachment_id, quality: nil)
+      attachment = ActiveStorage::Attachment.find(attachment_id)
 
-    return unless attachment
+      return unless attachment
 
-    preview = attachment.variant(resize_to_limit: [100, 100], format: :webp).processed
+      preview = attachment.variant(resize_to_limit: [100, 100], format: :webp, quality: quality).processed
 
-    image = Vips::Image.new_from_buffer(preview.download, "")
+      image = Vips::Image.new_from_buffer(preview.download, "")
 
-    attachment.blob.update!(preview_hash: preview_hash(image), primary_color: average_color_from_image(image))
-  end
-
-  private
-
-  def preview_hash(image)
-    if image.bands == 3
-      image = image.bandjoin(255)
+      attachment.blob.update!(preview_hash: preview_hash(image), primary_color: average_color_from_image(image))
     end
 
-    flattened_pixels = image.write_to_memory.unpack("C*")
+    private
 
-    ThumbHash.rgba_to_thumb_hash(
-      image.width,
-      image.height,
-      flattened_pixels
-    ).unpack1("H*")
+    def preview_hash(image)
+      if image.bands == 3
+        image = image.bandjoin(255)
+      end
+
+      flattened_pixels = image.write_to_memory.unpack("C*")
+
+      ThumbHash.rgba_to_thumb_hash(
+        image.width,
+        image.height,
+        flattened_pixels
+      ).unpack1("H*")
+    end
+
+    def average_color_from_image(image)
+      red_band = image[0].avg
+      green_band = image[1].avg
+      blue_band = image[2].avg
+
+      [red_band, green_band, blue_band].map(&:to_i).map { |value| value.to_s(16).rjust(2, "0") }.join.upcase
+    end
   end
-
-  def average_color_from_image(image)
-    red_band = image[0].avg
-    green_band = image[1].avg
-    blue_band = image[2].avg
-
-    [red_band, green_band, blue_band].map(&:to_i).map { |value| value.to_s(16).rjust(2, "0") }.join.upcase
-  end
-end
