78eed095de3a79a9efb55038921879a3fb60674b
Justin Marsh <jmarsh24@gmail.com>
2023-10-07 22:15:12 +0200

    update migration

--- db/migrate/20231007184935_add_search_text_to_dancer_and_user.rb
+++ db/migrate/20231007184935_add_search_text_to_dancer_and_user.rb
@@ -5,12 +5,19 @@ class AddSearchTextToDancerAndUser < ActiveRecord::Migration[7.1]
     add_column :dancers, :match_terms, :text, array: true, default: []
 
     Dancer.find_each do |dancer|
-      next unless dancer.nick_name
+      next unless dancer.nick_name && dancer.nick_name.length > 2
 
       nicknames_array = dancer.nick_name[1..-2].split(",").map do |item|
-        item.tr('"', "")
+        if item.is_a?(Array)
+          item.first.tr('"', "").strip
+        else
+          item.tr('"', "").strip
+        end
       end
-      dancer.match_terms = nicknames_array&.map { Dancer.normalize(_1) }
+
+      next unless nicknames_array.all? { |name| name.is_a?(String) }
+
+      dancer.match_terms = nicknames_array.map { |name| Dancer.normalize(name) }
       dancer.save!
     end
 
