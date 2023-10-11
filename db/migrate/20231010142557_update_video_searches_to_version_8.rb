5adff885163c80eab97b9cf26fc4fad3793ced47
Justin Marsh <jmarsh24@gmail.com>
2023-10-11 02:31:32 +0200

    add statement timeout

--- db/migrate/20231010142557_update_video_searches_to_version_8.rb
+++ db/migrate/20231010142557_update_video_searches_to_version_8.rb
@@ -1,6 +1,8 @@
 class UpdateVideoSearchesToVersion8 < ActiveRecord::Migration[7.1]
   def change
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 0")
     update_view :video_searches, version: 8, revert_to_version: 7, materialized: true
     add_index :video_searches, :search_text, using: :gin, opclass: :gin_trgm_ops
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO 60000")
   end
 end
