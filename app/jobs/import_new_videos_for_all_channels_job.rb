c138f2425b63425b1e9ba374008c30948172e5c3
Justin Marsh <jmarsh24@gmail.com>
2023-10-17 20:25:11 +0200

    sync video channels fix

--- app/jobs/import_new_videos_for_all_channels_job.rb
+++ app/jobs/import_new_videos_for_all_channels_job.rb
@@ -5,7 +5,7 @@ class ImportNewVideosForAllChannelsJob < ApplicationJob
 
   def perform
     Channel.active.find_each do |channel|
-      SyncVideosForChannelJob.perform_later(channel.id, use_music_recognizer: false)
+      SyncVideosForChannelJob.perform_later(channel.id, use_scraper: false, use_music_recognizer: true)
     end
   end
 end
