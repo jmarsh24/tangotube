4f9c6f8d608e0a3e7179b77556d80c85ed5d40a4
Justin Marsh <jmarsh24@gmail.com>
2023-10-22 22:05:44 +0200

    fix home spec

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -5,36 +5,20 @@ require "system_helper"
 RSpec.describe "homepage", type: :system do
   fixtures :all
 
-  context "banner" do
-    xit "displays the banner" do
-      visit root_path
-      expect(page).to have_content("The new way to watch Tango")
+  it "displays the banner" do
+    visit root_path
+    expect(page).to have_content("The new way to watch Tango")
 
-      screenshot "homepage_banner_present"
-    end
+    screenshot "homepage_banner_present"
   end
 
-  context "videos" do
-    before do
-      VideoSearch.refresh
-      page.driver.set_cookie("banner_closed", "true")
-      visit root_path
-      accept_all_button = find(".personalization-request__content a.button.button--primary", text: "Accept all")
-      accept_all_button.click
-    end
+  it "renders feedback" do
+    VideoScore.refresh
+    page.driver.set_cookie("banner_closed", "true")
+    visit root_path
 
-    xit "displays featured videos" do
-      skip "Waiting for videosearch to be refactored"
-      featured_videos = [videos(:video_1_featured), videos(:video_2_featured), videos(:video_3_featured), videos(:video_4_featured)]
+    expect(page).to have_content("tango")
 
-      screenshot "homepage_videos_present"
-
-      featured_videos.each do |video|
-        expect(page).to have_content(video.song.full_title)
-        video.dancers.each do |dancer|
-          expect(page).to have_content(dancer.name)
-        end
-      end
-    end
+    screenshot "homepage_videos_present"
   end
 end
