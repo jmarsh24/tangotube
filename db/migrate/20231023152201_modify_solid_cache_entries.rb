1a38f700abdcdedd4ce4bbb409778293193147e6
Justin Marsh <jmarsh24@gmail.com>
2023-10-23 17:30:58 +0200

    unlog and use hash index

---
+++ db/migrate/20231023152201_modify_solid_cache_entries.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ModifySolidCacheEntries < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  def change
+    safety_assured {
+      execute "ALTER TABLE solid_cache_entries SET UNLOGGED;"
+    }
+
+    add_index :solid_cache_entries, :key, using: "hash", algorithm: :concurrently
+  end
+end
