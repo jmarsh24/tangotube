e950ab22d21f4f0429940e578d2c50d8743b2ce9
Justin Marsh <jmarsh24@gmail.com>
2023-10-23 19:56:45 +0200

    update migration

--- db/migrate/20231010142557_update_video_searches_to_version_8.rb
+++ db/migrate/20231010142557_update_video_searches_to_version_8.rb
@@ -4,6 +4,7 @@ class UpdateVideoSearchesToVersion8 < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
+    update_view :video_scores, version: 8, revert_to_version: 7, materialized: true
     add_index :video_searches, :search_text, using: :gin, opclass: :gin_trgm_ops, algorithm: :concurrently
   end
 end
