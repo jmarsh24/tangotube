a1520ce9e6d9b66bd0ee5616a602dd3a030bf0f5
Justin Marsh <jmarsh24@gmail.com>
2023-10-26 22:16:32 +0200

    update

--- db/migrate/20231026193042_remove_solid_cache_key_hash_index.rb
+++ db/migrate/20231026193042_remove_solid_cache_key_hash_index.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class RemoveSolidCacheKeyHashIndex < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
   def change
     remove_index :solid_cache_entries, :key
     add_index :solid_cache_entries, :key, unique: true, algorithm: :concurrently
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_23_152201) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_193042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_stat_statements"
@@ -360,7 +360,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_23_152201) do
     t.binary "key", null: false
     t.binary "value", null: false
     t.datetime "created_at", null: false
-    t.index ["key"], name: "index_solid_cache_entries_on_key"
+    t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
   end
 
   create_table "songs", force: :cascade do |t|
