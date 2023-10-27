9c8e51612f18b64f82eeeb7a2a9c2d06e17a5e5d
Justin Marsh <jmarsh24@gmail.com>
2023-10-27 23:23:43 +0200

    update

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,42 +1,40 @@
 # frozen_string_literal: true
 
-if Config.worker_max_threads?
-  Rails.application.configure do
-    config.good_job.preserve_job_records = true
-    config.good_job.retry_on_unhandled_error = false
-    config.good_job.on_thread_error = ->(exception) { Rails.error.report(exception) }
-    config.good_job.execution_mode = :external
-    config.good_job.queues = "*"
-    config.good_job.max_threads = ENV.fetch("WORKER_MAX_THREADS") { 3 }
-    config.good_job.poll_interval = 5 # seconds
-    config.good_job.shutdown_timeout = 60 # seconds
-    config.good_job.enable_cron = true
-    config.good_job.dashboard_default_locale = :en
-    config.good_job.cron = {
-      channel_video_sync: {
-        cron: "0 5 * * *",
-        class: "SyncAllChannelVideosJob"
-      },
-      update_active_channels_job: {
-        cron: "0 3 * * *",
-        class: "UpdateActiveChannelsJob"
-      },
-      sitemap: {
-        cron: "0 0 12 * *",
-        class: "Shimmer::SitemapJob"
-      },
-      refresh_video_searches_view: {
-        cron: "0 * * * *", # every hour
-        class: "RefreshVideoSearchesViewJob"
-      },
-      refresh_video_scores_scores: {
-        cron: "*/30 * * * *", # every 10 minutes
-        class: "RefreshVideoScoresJob"
-      },
-      refresh_query_stats: {
-        cron: "*/5 * * * *", # every 5 minutes
-        class: "CaptureQueryStatsJob"
-      }
+Rails.application.configure do
+  config.good_job.preserve_job_records = true
+  config.good_job.retry_on_unhandled_error = false
+  config.good_job.on_thread_error = ->(exception) { Rails.error.report(exception) }
+  config.good_job.execution_mode = :external
+  config.good_job.queues = "*"
+  config.good_job.max_threads = ENV.fetch("WORKER_MAX_THREADS") { 3 }
+  config.good_job.poll_interval = 5 # seconds
+  config.good_job.shutdown_timeout = 60 # seconds
+  config.good_job.enable_cron = true
+  config.good_job.dashboard_default_locale = :en
+  config.good_job.cron = {
+    channel_video_sync: {
+      cron: "0 5 * * *",
+      class: "SyncAllChannelVideosJob"
+    },
+    update_active_channels_job: {
+      cron: "0 3 * * *",
+      class: "UpdateActiveChannelsJob"
+    },
+    sitemap: {
+      cron: "0 0 12 * *",
+      class: "Shimmer::SitemapJob"
+    },
+    refresh_video_searches_view: {
+      cron: "0 * * * *", # every hour
+      class: "RefreshVideoSearchesViewJob"
+    },
+    refresh_video_scores_scores: {
+      cron: "*/30 * * * *", # every 10 minutes
+      class: "RefreshVideoScoresJob"
+    },
+    refresh_query_stats: {
+      cron: "*/5 * * * *", # every 5 minutes
+      class: "CaptureQueryStatsJob"
     }
-  end
+  }
 end
