6e27daeece7a2ac42d8692800ccc9500d6396384
Justin Marsh <jmarsh24@gmail.com>
2023-11-13 19:54:54 +0100

    try string with ;

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.good_job.retry_on_unhandled_error = false
   config.good_job.on_thread_error = ->(exception) { Rails.error.report(exception) }
   config.good_job.execution_mode = :external
-  config.good_job.queues = "*"
+  config.good_job.queues = "update:5;"
   config.good_job.max_threads = ENV.fetch("WORKER_MAX_THREADS", 3)
   config.good_job.poll_interval = 5 # seconds
   config.good_job.shutdown_timeout = 60 # seconds
