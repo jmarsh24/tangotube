99c74f8e9207a99fa04299dc016ae7b854fb847a
Justin Marsh <jmarsh24@gmail.com>
2023-11-14 10:26:16 +0100

    when song is updated refind new video matches

--- app/controllers/avo/songs_controller.rb
+++ app/controllers/avo/songs_controller.rb
@@ -3,4 +3,12 @@
 # This controller has been generated to enable Rails' resource routes.
 # More information on https://docs.avohq.io/2.0/controllers.html
 class Avo::SongsController < Avo::ResourcesController
+  def update_success_action
+    videos = resource.model.videos
+    videos.update_all(song_id: nil)
+    videos.each do |video|
+      UpdateVideoJob.perform_later(video)
+    end
+    super
+  end
 end
