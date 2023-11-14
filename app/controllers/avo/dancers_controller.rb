e9b0dca53ae28b0695d17ae2742d405aa7ada2a1
Justin Marsh <jmarsh24@gmail.com>
2023-11-14 10:05:43 +0100

    update video matches for dancers after create update

--- app/controllers/avo/dancers_controller.rb
+++ app/controllers/avo/dancers_controller.rb
@@ -3,4 +3,13 @@
 # This controller has been generated to enable Rails' resource routes.
 # More information on https://docs.avohq.io/2.0/controllers.html
 class Avo::DancersController < Avo::ResourcesController
+  def create_success_action
+    resource.model.update_video_matches
+    super
+  end
+
+  def update_success_action
+    resource.model.update_video_matches
+    super
+  end
 end
