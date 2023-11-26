0db3e4392fd0cb55b4d71bdb0b44759c19a27245
Justin Marsh <jmarsh24@gmail.com>
2023-11-26 14:59:27 +0100

    Fix watch controller

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -3,28 +3,24 @@
 class WatchesController < ApplicationController
   # @route GET /watch (watch)
   def show
-    redirect_to root_path unless params[:v].present?
-    @video = Video.find_by(youtube_id: params[:v])
+    if params[:v].blank?
+      redirect_to root_path and return
+    end
 
-    if @video.nil?
-      @video = Import::Importer.new.import(params[:v])
-      if @video.present?
-        UpdateVideoJob.perform_later(@video, use_music_recognizer: true)
-      else
-        flash[:error] = "Video not found"
-        redirect_to root_path
-      end
-    else
+    @video = Video.find_by(youtube_id: params[:v]) || Import::Importer.new.import(params[:v])
+
+    if @video
+      UpdateVideoJob.perform_later(@video, use_music_recognizer: true)
       @type = Video::RelatedVideos.new(@video).available_types.first
-
       @playback_options = Watch::PlaybackOptions.new(
         start_time: params[:start],
         end_time: params[:end],
         speed: params[:speed] || "1"
       )
-      unless is_bot?
-        current_user&.watches&.create!(video: @video, watched_at: Time.now)
-      end
+      current_user&.watches&.create!(video: @video, watched_at: Time.now)
+    else
+      flash[:error] = "Video not found"
+      redirect_to root_path and return
     end
   end
 end
