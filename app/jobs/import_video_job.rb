bc7ae3d831cd2c1f3eea294977d7fe9cc2bcb36a
Justin Marsh <jmarsh24@gmail.com>
2023-12-11 21:21:49 +0100

    update import job

--- app/jobs/import_video_job.rb
+++ app/jobs/import_video_job.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ImportVideoJob < ApplicationJob
-  def perform(youtube_slug, use_scraper: true, use_music_recognizer: true)
-    self.class.queue_as use_music_recognizer ? :music_recognizer_queue : :update
+  queue_as :music_recognizer
 
-    Import::Importer.new.import(youtube_slug, use_scraper:, use_music_recognizer:)
+  def perform(youtube_slug)
+    Import::Importer.new.import(youtube_slug, use_music_recognizer: true)
   end
 end
